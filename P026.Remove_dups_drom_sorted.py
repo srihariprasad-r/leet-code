145bd5057cb84e779f67db9dce5dc6353aead1b0
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-07-06 00:24:21 +0530

    added solution for leetcode solution of P26

---
+++ P026.Remove_dups_drom_sorted.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        index = 1;
+        for i in range(0, len(nums)-1):
+            if (nums[i] != nums[i+1]):
+                nums[index] = nums[i+1]
+                index += 1
+        
+        return index
