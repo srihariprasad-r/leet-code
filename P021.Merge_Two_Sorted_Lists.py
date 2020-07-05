d25d66f2c8325ab4937c7f7530eaf5fd43cae6b2
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-07-05 23:48:59 +0530

    added leetcode solution of merging sorted lists

---
+++ P021.Merge_Two_Sorted_Lists.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        l3 = ListNode(-1);
+        head = l3
+        while(not(l1 is None) and not(l2 is None)):
+            if(l1.val <= l2.val):
+                l3.next = l1
+                l1 = l1.next
+                #l3 = l3.next
+            else:
+                l3.next = l2
+                l2 = l2.next
+            l3 = l3.next
+                
+            if (l1 is not None):
+                l3.next = l1;
+            elif (l2 is not None):
+                l3.next = l2;
+        
+        return head.next
