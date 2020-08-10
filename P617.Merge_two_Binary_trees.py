e3ca6293fa9345e35bf2f19bb57fd406fdca6974
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-08-11 00:41:16 +0530

    added solution for problem 617

---
+++ P617.Merge_two_Binary_trees.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def mergeTrees(self, t1, t2):
+        """
+        :type t1: TreeNode
+        :type t2: TreeNode
+        :rtype: TreeNode
+        """
+        if t1 is None:
+            return t2
+        elif t2 is None:
+            return t1
+        else:
+            t1.val = t1.val + t2.val
+            t1.left = self.mergeTrees(t1.left, t2.left)
+            t1.right = self.mergeTrees(t1.right, t2.right)
+        return t1
