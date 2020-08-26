371528ff9bf7f62e263091bb3c67f7e39b63da99
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-08-27 00:39:28 +0530

    added solution for problem 237

---
+++ P237.Delete_node_linked_list.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        deletes = node.val
+        
+        while node:
+            if node.val == deletes:
+                node.val = node.next.val
+                node.next = node.next.next
+            else:
+                node = node.next
