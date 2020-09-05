c9db9622fe4c94b23ad6845bb663dc0e21a7f69d
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-09-05 20:22:09 +0530

    updated working version of contest 204 Q#2

--- Weeky_contest_204/Maximum_product_subarray.py
+++ Weeky_contest_204/Maximum_product_subarray.py
@@ -4,43 +4,25 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        max_prod = 1
-        tmp = 1
         
-        start, end = 0, 0 
+        pos = [0] * len(nums)
+        neg = [0] * len(nums)        
         
-        while nums and nums[0] == 0:
-            nums = nums[1:]
-
-        if len(nums) == 0:
-            return 0
-        elif len(nums) == 1 and nums[0] > 0:
-            return 1
-        elif len(nums) == 1 and nums[0] < 0:
-            return 0
-                                
-        for i in range(len(nums)):
-            if nums[i] == 0:
-                if i + 1 < len(nums) and  max_prod < max_prod * nums[i+1]:
-                    start = i + 1
-                    tmp = 1                                
-                    max_prod = 1
-                    continue                    
-                elif i + 1 == len(nums):
-                    return end - start + 1
-                                    
-            tmp  = tmp * nums[i]                                                            
+        if nums[0] > 0:
+            pos[0] = 1
+        elif nums[0] < 0:
+            neg[0] = 1
+        
+        result = pos[0]
+        
+        for i in range(1, len(nums)):
+            if nums[i] > 0:
+                pos[i] = 1 + pos[i-1]
+                neg[i] = 1 + neg[i-1] if neg[i-1] > 0 else 0
+            elif nums[i] < 0:
+                pos[i] = 1 + neg[i-1] if neg[i-1] > 0 else 0                
+                neg[i] = 1 + pos[i-1] 
             
-            if tmp > 0 and max_prod < tmp:    
-                max_prod = tmp 
-                end = i    
-            elif tmp < 0 and max_prod < tmp:                
-                start = i + 1
-                tmp = 1
-                max_prod = 1
-            elif tmp > 0 and max_prod > tmp:
-                end= i + 1
-                tmp = 1
-                max_prod = 1                                                        
-            
-        return end - start + 1
+            result = max(result, pos[i])
+        
+        return result
