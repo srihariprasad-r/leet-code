0e54db7c46f29194a542db996e6095005249da36
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2020-09-22 19:01:57 +0530

    added solution for P746

---
+++ P746.Min_cost_climibing_stairs.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def minCostClimbingStairs(self, cost):
+        """
+        :type cost: List[int]
+        :rtype: int
+        """
+        dp = [0] * len(cost)
+        
+        dp[0] = cost[0]
+        dp[1] = cost[1]
+        
+        for i in range(2, len(cost)):
+            dp[i] = cost[i] + min(dp[i-1], dp[i-2])
+                
+        return min(dp[-2], dp[-1])
+        
