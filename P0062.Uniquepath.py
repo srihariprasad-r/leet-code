bfac3beadca003fbe36a0ce1f749adf0b61aea62
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2021-02-06 19:07:12 +0530

    added solution for P62

---
+++ P0062.Uniquepath.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        dp = [[-1 for i in range(n)] for j in range(m)]
+    
+        for i in range(m):
+            for j in range(n):
+                if i == 0 or j == 0:
+                    dp[i][j] = 1
+                else:
+                    dp[i][j] = dp[i][j-1] +  dp[i-1][j]
+                
+        return dp[-1][-1]
