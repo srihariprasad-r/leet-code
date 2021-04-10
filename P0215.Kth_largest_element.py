dbab795e6a90b0d5bbb5d9990db08d099407b596
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2021-04-10 22:27:25 +0530

    added solution for P215

---
+++ P0215.Kth_largest_element.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        nums = [-x for x in nums]
+        heapq.heapify(nums)
+        
+        for i in range(k):
+            res = -heapq.heappop(nums)
+        
+        return res
