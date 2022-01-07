64b898b31f1d5ed9b5b5efbb0353b2bffc3f7dac
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-01-07 18:11:19 +0530

    added solution for LC382

---
+++ 2022/01 January monthly challenge/P0382.Linked_list_random_node.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+
+    def __init__(self, head):
+        """
+        :type head: Optional[ListNode]
+        """
+        self.lst = []
+        
+        while head:
+            self.lst.append(head.val)
+            head = head.next 
+
+    def getRandom(self):
+        """
+        :rtype: int
+        """
+        return random.choice(self.lst)
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(head)
+# param_1 = obj.getRandom()
