593738db0ff3eb304c46d42456d4f27e5f1486a2
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-02-15 19:04:18 +0530

    added solution for minimum alternating array

--- Weekly_contest_230/Min_operations_array_alternating.py
+++ Weekly_contest_230/Min_operations_array_alternating.py
@@ -6,18 +6,28 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        cnt, i = 0, 0
-        stck = []
-        stck.append(nums[i])
+        odd_map, even_map = {}, {}
+        max_v_odd, max_v_even = -float('inf'),  -float('inf')
+        second_flag = False
         
-        while i <= len(nums) - 2 - 1:
-            j , k = i + 2, i + 1
-            if not(stck[-1] != nums[k] and stck[-1] == nums[j]):
-                if stck[-1] != nums[j]: nums[j] = stck[-1]
-                cnt += 1
-            if stck:
-                stck.pop()
-                stck.append(nums[k])
-            i += 1
+        for i in range(len(nums)):
+            if i % 2 == 0:
+                if not(nums[i] in even_map):
+                    even_map[nums[i]] = 1
+                else:
+                    even_map[nums[i]] += 1
+            else:
+                if not(nums[i] in odd_map):
+                    odd_map[nums[i]] = 1
+                else:
+                    odd_map[nums[i]] += 1                
+        
+        max_v_odd = max([v for v in odd_map.values()])
+        max_v_even = max([v for v in even_map.values()])
+        
+        if max_v_odd == max_v_even:
+            second_flag = True
+            new_even_map = {k:v for k, v in even_map.items() if v != max_v_even}
+            new_v_even = max([v for v in new_even_map.values()])
             
-        return cnt
+        return len(nums) - max(max_v_odd + new_v_even, new_v_even + max_v_even) if second_flag else len(nums) - (max_v_odd + max_v_even)
