9ed37f8b56b22d1a98c1ccda0cc2c06acc032b16
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-02-22 22:19:38 +0530

    added solution for P167 method 2

--- P167.Two_Sum_II_Input_array_sorted.py
+++ P167.Two_Sum_II_Input_array_sorted.py
@@ -27,3 +27,24 @@ class Solution(object):
                 
         
         return res[0] if len(res) > 0 else res
+
+
+# Method 2
+
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        left = 0
+        right = len(numbers) - 1
+
+        while left != right:
+            if numbers[left] + numbers[right] == target:
+                return [left+1, right + 1]
+            elif numbers[left] + numbers[right] > target:
+                right -= 1
+            else:
+                left += 1
