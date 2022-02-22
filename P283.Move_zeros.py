3d5632bd75509ba56ba3b698eacca40000cb11d5
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-02-22 22:50:00 +0530

    added solution for P283 method 2

--- P283.Move_zeros.py
+++ P283.Move_zeros.py
@@ -8,3 +8,28 @@ class Solution(object):
             return nums.sort(key=lambda x: 1 if x == 0 else 0)
         else:
             return nums                
+
+
+# Method 2
+
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        left = 0
+        right = 1
+
+        while right < len(nums):
+            if nums[left] != 0:
+                left += 1
+                right += 1
+            elif nums[right] == 0:
+                right += 1
+            else:
+                tmp = nums[right]
+                nums[right] = nums[left]
+                nums[left] = tmp
+
+        return nums
