9d8b23e324215ed6dc90f1258ddce39d5be119cb
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-03-01 22:22:47 +0530

    added solution for P2149

---
+++ P2149.Rearrange_array.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def rearrangeArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        left = 0
+        right = 1
+        i = j = 0
+
+        while right < len(nums):
+            if nums[right] > 0:
+                j = left
+                while j < len(nums):
+                    if nums[j] < 0:
+                        break
+                    j += 1
+                if j < len(nums) and right < len(nums):
+                    nums[j], nums[right] = nums[right], nums[j]
+                right += 2
+            elif nums[left] < 0:
+                i = right
+                while i < len(nums):
+                    if nums[i] > 0:
+                        break
+                    i += 1
+                if i < len(nums) and left < len(nums):
+                    nums[i], nums[left] = nums[left], nums[i]
+                left += 2
+            else:
+                left += 2
+                right += 2
+
+        return nums
