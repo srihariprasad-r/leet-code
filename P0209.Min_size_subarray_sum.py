f76172a38b5a5aa7055027c46a4785111111bc0f
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-03-21 23:33:32 +0530

    added solution for P209

---
+++ P0209.Min_size_subarray_sum.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def minSubArrayLen(self, target, nums):
+        """
+        :type target: int
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        st, rsum = 0, 0
+
+        mlen = float('inf')
+
+        for i in range(len(nums)):
+            rsum += nums[i]
+            while rsum >= target:
+                mlen = min(mlen, i - st+1)
+                rsum -= nums[st]
+                st += 1
+
+        return 0 if mlen == float('inf') else mlen
