1da041d0375ada44a0fee1eaf53512ab54684460
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-03-30 21:47:11 +0530

    update for P34

--- P0034.First_last_element_array.py
+++ P0034.First_last_element_array.py
@@ -1,5 +1,3 @@
-# Wrong submission
-
 class Solution(object):
     def searchRange(self, nums, target):
         """
@@ -9,25 +7,32 @@ class Solution(object):
         """
         if len(nums) == 0:
             return [-1, -1]
-        elif len(nums) == 1 and nums[0] == target:
-            return [0, 0]
 
-        left = 0
-        right = len(nums) - 1
-
-        while left < right:
-            mid = left + (right - left)//2
-            if nums[mid] == target:
-                if mid - 1 >= 0 and nums[mid-1] == target:
-                    return [mid-1, mid]
-                elif mid + 1 < len(nums) and nums[mid+1] == target:
-                    return [mid, mid+1]
+        def searchleft(nums):
+            left = 0
+            right = len(nums) - 1
+            while left < right:
+                mid = left + (right-left)//2
+                if nums[mid] < target:
+                    left = mid + 1
                 else:
-                    return [mid, mid]
+                    right = mid
 
-            if nums[mid] < target:
-                left = mid + 1
-            else:
-                right = mid - 1
+            return left
 
-        return [right, right] if nums[right] == target else [-1, -1]
+        def searchright(nums):
+            left = 0
+            right = len(nums)
+            while left < right:
+                mid = left + (right-left)//2
+                if nums[mid] > target:
+                    right = mid
+                else:
+                    left = mid + 1
+
+            return left
+
+        lt = searchleft(nums)
+        if lt == len(nums) or nums[lt] != target:
+            return [-1, -1]
+        return [lt, searchright(nums)-1]
