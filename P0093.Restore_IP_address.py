07a1eb12aa88d845f0160350858e8c022b421b9b
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-04-01 00:42:34 +0530

    added solution for P93

---
+++ P0093.Restore_IP_address.py
@@ -0,0 +1,42 @@
+class Solution(object):
+    def restoreIpAddresses(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        import copy
+
+        def valid(s):
+            if len(s) > 3 or len(s) < 1:
+                return False
+            elif s[0] == '0' and len(s) > 1:
+                return False
+            elif int(s) > 255:
+                return False
+
+            return True
+
+        def helper(s, idx, dots, arr=[], res=[]):
+            if dots > 3 or idx >= len(s):
+                return
+
+            if dots == 3:
+                if valid(s[idx:]):
+                    arr.append('.' + s[idx:])
+                    res.append(''.join(copy.deepcopy(arr)))
+                    return
+
+            for i in range(idx, len(s)):
+                if valid(s[idx:i+1]):
+                    if dots == 0:
+                        tmp = s[idx:i+1]
+                    else:
+                        tmp = str('.' + s[idx:i+1])
+
+                    arr.append(tmp)
+                    helper(s, i+1, dots + 1, arr, res)
+                    arr.pop()
+
+            return res
+
+        return helper(s, 0, 0)
