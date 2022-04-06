34c9f17397d335d1d471153e0a4fdc063b406ce0
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-04-06 23:49:50 +0530

    added solution for P801

---
+++ P801.Minimum_swaps_increasing_array.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def minSwap(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: int
+        """
+        def backtrack(idx, ln, swaps, ans=float('inf')):
+            if idx == ln:
+                ans = min(ans, swaps)
+                return ans
+
+            if nums1[idx] > nums1[idx-1] and nums2[idx] > nums2[idx-1]:
+                ans = min(ans, backtrack(idx+1, ln, swaps, ans))
+
+            if nums1[idx] > nums2[idx-1] and nums2[idx] > nums1[idx-1]:
+                nums1[idx], nums2[idx] = nums2[idx], nums1[idx]
+                ans = min(ans, backtrack(idx+1, ln, swaps+1, ans))
+                nums1[idx], nums2[idx] = nums2[idx], nums1[idx]
+
+            return ans
+
+        n = len(nums1)
+        return backtrack(1, n, 0)
