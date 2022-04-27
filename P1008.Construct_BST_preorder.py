dd0fc4f809dfd03f7c2e0b56ebf88b0f41bbab53
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-04-27 21:37:11 +0530

    updated solution for P1008

--- P1008.Construct_BST_preorder.py
+++ P1008.Construct_BST_preorder.py
@@ -27,3 +27,50 @@ class Solution(object):
             root = f(root, e)
         
         return root
+
+# Method 2
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution(object):
+    def bstFromPreorder(self, preorder):
+        """
+        :type preorder: List[int]
+        :rtype: TreeNode
+        """
+        def f(lst, n, pos, node, left_range, right_range):
+            if pos == n or lst[pos] < left_range or lst[pos] > right_range:
+                return pos
+
+            if lst[pos] < node.val:
+                node.left = TreeNode(lst[pos])
+                pos += 1
+                pos = f(lst, n, pos, node.left, left_range, node.val - 1)
+
+            if pos == n or lst[pos] < left_range or lst[pos] > right_range:
+                return pos
+
+            node.right = TreeNode(lst[pos])
+            pos += 1
+            pos = f(lst, n, pos, node.right, node.val + 1, right_range)
+
+            return pos
+
+        if len(preorder) == 0:
+            return None
+
+        n = len(preorder)
+        root = TreeNode(preorder[0])
+
+        if n == 1:
+            return root
+
+        f(preorder, n, 1, root, float('-inf'), float('inf'))
+
+        return root
