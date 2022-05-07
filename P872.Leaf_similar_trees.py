3e5e6a4fca4046fa3c3c8a5aa5c05f4805c455d0
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-05-07 12:29:06 +0530

    added solution for P872

--- P872.Leaf_similar_trees.py
+++ P872.Leaf_similar_trees.py
@@ -23,4 +23,38 @@ class Solution(object):
         :rtype: bool
         """
         return self.leafValue(root1) == self.leafValue(root2)
-        
+
+# Method 2
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def leafSimilar(self, root1, root2):
+        """
+        :type root1: TreeNode
+        :type root2: TreeNode
+        :rtype: bool
+        """
+        def helper(node):
+            q = deque()
+            q.append(node)
+
+            ans = []
+
+            while len(q) > 0:
+                el = q.pop()
+                if not el.left and not el.right:
+                    ans.append(el.val)
+
+                if el.left:
+                    q.append(el.left)
+
+                if el.right:
+                    q.append(el.right)
+
+            return ans
+        return helper(root1) == helper(root2)
