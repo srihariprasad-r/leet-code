007554ed7bed6fc1dd3c1f5d0a7f5a9d25142b61
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-05-08 20:05:24 +0530

    updated solution for P814

--- P0814.Binary_tree_pruning.py
+++ P0814.Binary_tree_pruning.py
@@ -1,5 +1,3 @@
-# Wrong submission
-
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
@@ -15,25 +13,10 @@ class Solution(object):
         if root is None:
             return None
 
-        if root.val == 0:
-            if (root.left and root.left.val == 0) and (root.right and root.right.val == 0):
-                return None
-
-            if not root.left and not root.right:
-                return None
-
-            # if ((not root.left and root.right and root.right.val == 0)\
-            #     or (not root.right and root.left and root.left.val == 0)):
-            #     return None
-
-            # if root.left and root.left.val == 1:
-            #     return self.pruneTree(root.left)
-            # elif root.right and root.right.val == 1:
-            #     return self.pruneTree(root.right)
-            # else:
-            #     return None
-
         root.left = self.pruneTree(root.left)
         root.right = self.pruneTree(root.right)
 
-        return root
+        if (root.val == 1) or (root.val == 0 and (root.left or root.right)):
+            return root
+        else:
+            return None
