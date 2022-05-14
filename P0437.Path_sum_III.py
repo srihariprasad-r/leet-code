9a4ea4a26db80776b9de10ec1dc90ea78fc17e07
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-05-14 22:46:33 +0530

    added solution for P437

---
+++ P0437.Path_sum_III.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def pathSum(self, root, targetSum):
+        """
+        :type root: TreeNode
+        :type targetSum: int
+        :rtype: int
+        """
+        self.result = 0
+        self.sum_cnt = defaultdict(int)
+        self.sum_cnt[0] = 1
+        
+        def dfs(node, running_sum):
+            if not node:
+                return self.result
+            
+            running_sum += node.val
+            
+            if (running_sum - targetSum) in self.sum_cnt:
+                self.result += self.sum_cnt[running_sum - targetSum]
+            
+            self.sum_cnt[running_sum- targetSum] += 1
+            
+            dfs(node.left, running_sum)
+            dfs(node.right, running_sum)
+            
+            return self.result
+        
+        return dfs(root, 0)
