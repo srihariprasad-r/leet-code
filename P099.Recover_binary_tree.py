973bc517bcbaa39021b7ef4b1458cb91230542ef
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-05-14 20:33:40 +0530

    updated solution for P99

--- P099.Recover_binary_tree.py
+++ P099.Recover_binary_tree.py
@@ -35,3 +35,41 @@ class Solution(object):
         first.val, second.val = second.val, first.val
 
         return arr
+
+# Method 2
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def recoverTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: None Do not return anything, modify root in-place instead.
+        """
+        self.first = self.pre = self.second = None
+        
+        def inorder(node):
+            if not node:
+                return
+            
+            inorder(node.left)
+            
+            if self.pre and node.val < self.pre.val:
+                self.first = node
+                
+                if not self.second:
+                    self.second = self.pre
+                else:
+                    return
+
+            self.pre =  node
+
+            inorder(node.right)
+            
+        inorder(root)
+
+        self.first.val, self.second.val = self.second.val, self.first.val
