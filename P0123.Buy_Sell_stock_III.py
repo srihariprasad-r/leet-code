77fb40af05f79275261d8712c3fce248ac801785
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-05-31 21:52:24 +0530

    added solution for P123

---
+++ P0123.Buy_Sell_stock_III.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        profit = 0
+        left = right = [0] * len(prices)
+        minleft = prices[0]
+
+        for i in range(1, len(prices)):
+            left[i] = max(left[i-1], prices[i] - minleft)
+            minleft = min(minleft, prices[i])
+
+        maxright = prices[-1]
+
+        for i in range(len(prices)-2, -1, -1):
+            right[i] = max(right[i+1], maxright - prices[i])
+            maxright = max(maxright, prices[i])
+
+        profit = right[0]
+        for i in range(1, len(prices)):
+            profit = max(profit, left[i-1]+right[i])
+
+        return profit
