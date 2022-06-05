2c65e5f88feaed9401e0a0bcb15ed76e18ab35b4
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-06-05 20:00:54 +0530

    added solution for P1574

--- P1574.Shortest_subarray_removal_make_array_sorted.py
+++ P1574.Shortest_subarray_removal_make_array_sorted.py
@@ -10,18 +10,22 @@ class Solution(object):
         while peak < len(arr) - 1 and arr[peak+1] >= arr[peak]:
             peak += 1
             
+        if peak == len(arr) - 1:
+            return 0
+            
         while valley > peak and arr[valley-1] <= arr[valley]:
             valley -= 1
         
         ans = min(len(arr)-peak-1, valley)
 
-        i = peak
+        i = 0
         j = valley
         
-        while j < len(arr):
+        while i <= peak and j < len(arr):
             if arr[j] >= arr[i]:
                 ans = min(ans, j - i - 1)
-            j += 1
+                i += 1
+            else:
+                j += 1
             
         return ans
-            
