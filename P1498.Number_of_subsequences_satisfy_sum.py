6c386ba1b113ba53fe1de97916547254177be9fc
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-06-07 22:50:42 +0530

    added solution for P1498

--- P1498.Number_of_subsequences_satisfy_sum.py
+++ P1498.Number_of_subsequences_satisfy_sum.py
@@ -5,15 +5,17 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
+        nums.sort()
+        mod = 10**9+7
         n = len(nums)
         l = ans = 0
         r = n - 1
 
         while l <= r:
             if nums[l] + nums[r] <= target:
-                ans += 2 ** (r - l)
+                ans += pow(2, r - l, mod)
                 l += 1
             else:
                 r -= 1
 
-        return ans
+        return ans % mod
