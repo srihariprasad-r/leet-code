c7a201623196b768f9067ea98e2fd9e4a42e1ea3
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-06-17 21:33:50 +0530

    remove tmp file

--- P1664.Fairarray.py
+++ P1664.Fairarray.py
@@ -18,6 +18,8 @@ class Solution(object):
         return n
 
 # Method 2:
+
+
 class Solution(object):
     def waysToMakeFair(self, nums):
         """
@@ -26,26 +28,26 @@ class Solution(object):
         """
         n = len(nums)
         ans = 0
-        
+
         oddPreSum = [0] * (n+1)
         evenPreSum = [0] * (n+1)
-        
+
         for i in range(len(nums)):
             if i % 2:
                 oddPreSum[i+1] = oddPreSum[i] + nums[i]
                 evenPreSum[i+1] = evenPreSum[i]
             else:
                 oddPreSum[i+1] = oddPreSum[i]
-                evenPreSum[i+1] = evenPreSum[i]  + nums[i]
-                
+                evenPreSum[i+1] = evenPreSum[i] + nums[i]
+
         for i in range(1, n+1):
             oddSum1 = oddPreSum[i-1]
             oddSum2 = evenPreSum[-1] - evenPreSum[i]
-            
+
             evenSum1 = evenPreSum[i-1]
             evenSum2 = oddPreSum[-1] - oddPreSum[i]
-            
+
             if oddSum1 + oddSum2 == evenSum1 + evenSum2:
                 ans += 1
-                
+
         return ans
