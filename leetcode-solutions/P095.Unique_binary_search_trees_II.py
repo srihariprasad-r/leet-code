af7f17beba62d48f69c03c132bb890da9e103bd1
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-06-24 21:56:15 +0530

    added solution for P95

--- leetcode-solutions/P095.Unique_binary_search_trees_II.py
+++ leetcode-solutions/P095.Unique_binary_search_trees_II.py
@@ -34,3 +34,40 @@ class Solution(object):
                     lst.append(node)
                     
         return lst
+
+# Method 2
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def generateTrees(self, n):
+        """
+        :type n: int
+        :rtype: List[TreeNode]
+        """
+        def dfs(nums):
+            if not nums:
+                return [None]
+
+            ans = []
+
+            for i in range(len(nums)):
+                left = dfs(nums[:i])
+                right = dfs(nums[i+1:])
+
+                for l in left:
+                    for r in right:
+                        root = TreeNode(nums[i])
+                        root.left = l
+                        root.right = r
+                        ans.append(root)
+
+            return ans
+
+        nums = [i for i in range(1, n+1)]
+
+        return dfs(nums)
