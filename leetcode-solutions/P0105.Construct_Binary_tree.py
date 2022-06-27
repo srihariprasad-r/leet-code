511aed7f47f9e3ddeeb4303ddbd5ba6ee3f07e37
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-06-27 22:05:48 +0530

    added solution for P105

--- leetcode-solutions/P0105.Construct_Binary_tree.py
+++ leetcode-solutions/P0105.Construct_Binary_tree.py
@@ -20,3 +20,32 @@ class Solution(object):
         root.right = self.buildTree(preorder[mid+1:], inorder[mid+1:])
 
         return root
+
+# Method 2
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def buildTree(self, preorder, inorder):
+        """
+        :type preorder: List[int]
+        :type inorder: List[int]
+        :rtype: TreeNode
+        """
+        def dfs(l, r):
+            if l > r:
+                return None
+    
+            value = preorder.pop(0)
+            root = TreeNode(value)
+            idx = inorder.index(value)
+            root.left = dfs(l, idx-1)
+            root.right = dfs(idx+1, r)
+            
+            return root
+    
+        return dfs(0, len(preorder)- 1)
