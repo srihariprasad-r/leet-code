a2f1b12f84e89e6ef6983e907b2acbc6502d8eef
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-06-29 22:23:50 +0530

    added solution for P98

--- leetcode-solutions/P098.Validate_binary_tree.py
+++ leetcode-solutions/P098.Validate_binary_tree.py
@@ -28,3 +28,30 @@ class Solution(object):
                 s.append((el.right, el.val, right))
 
         return True
+
+
+# Method 2
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        def dfs(root, left, right):
+            if not root:
+                return True
+            
+            if root.val >= left and root.val <= right:
+                return dfs(root.left, left, root.val - 1) \
+                    and dfs(root.right, root.val + 1, right)
+            
+            return False
+        
+        return dfs(root, float('-inf'), float('inf'))
