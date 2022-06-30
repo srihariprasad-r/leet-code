57ff0f4378e7a8ee00c76c4fa6a061ed37a1c51e
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-06-30 20:06:42 +0530

    added solution for P144

--- leetcode-solutions/P144.Binary_tree_preorder_traversal.py
+++ leetcode-solutions/P144.Binary_tree_preorder_traversal.py
@@ -21,3 +21,34 @@ class Solution(object):
             return result
         
         return dfs(root)
+
+
+# Method 2
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root:
+            return []
+        
+        stck = [root]
+        ans = []
+        
+        while stck:          
+            el = stck.pop()
+            ans.append(el.val)
+            if el.right:
+                stck.append(el.right)
+            if el.left:
+                stck.append(el.left)
+        
+        return ans
+        
