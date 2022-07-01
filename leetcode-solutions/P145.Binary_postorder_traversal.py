9f93d2cc09bc2294762a00cfccb8606b7ccc50b3
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-07-01 20:05:46 +0530

    added solution for P145

--- leetcode-solutions/P145.Binary_postorder_traversal.py
+++ leetcode-solutions/P145.Binary_postorder_traversal.py
@@ -22,3 +22,34 @@ class Solution(object):
             return res
 
         return dfs(root)
+
+# Method 2
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def postorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root:
+            return []
+        
+        stck = [root]
+        ans = []
+        
+
+        while stck:
+            el = stck.pop()
+            ans.append(el.val)
+            if el.left:
+                stck.append(el.left)
+
+            if el.right:
+                stck.append(el.right)
+            
+        return ans[::-1]
