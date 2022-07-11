50daf55e0da7077cc8cb73ba8e27fb2527c3351f
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-07-11 22:18:59 +0530

    added solution for P2025

--- leetcode-solutions/P2025.Max_ways_partition_array.py
+++ leetcode-solutions/P2025.Max_ways_partition_array.py
@@ -7,28 +7,28 @@ class Solution(object):
         """
         presum = [0]
         ans = 0
-        
+
         for x in nums:
             presum.append(presum[-1]+x)
-        
+
         presum.pop(0)
 
         rightcnt = Counter()
         leftcnt = Counter()
-        
+
         for i in range(1, len(nums)):
-            diff = presum[i-1] - (presum[-1]- presum[i-1])
+            diff = presum[i-1] - (presum[-1] - presum[i-1])
             rightcnt[diff] += 1
-            
+
         ans = rightcnt[0]
-        
+
         for i in range(len(nums)):
             d = k - nums[i]
-            
+
             ans = max(ans, leftcnt[d] + rightcnt[-d])
-            
-            diff = presum[i] - (presum[-1]- presum[i])
+
+            diff = presum[i] - (presum[-1] - presum[i])
             rightcnt[diff] -= 1
             leftcnt[diff] += 1
-            
+
         return ans
