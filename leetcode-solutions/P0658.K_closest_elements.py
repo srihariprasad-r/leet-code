7fd938f9aa8c0da388a679c30edcc4f8322aeae7
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-07-26 22:02:04 +0530

    added solution for P658

--- leetcode-solutions/P0658.K_closest_elements.py
+++ leetcode-solutions/P0658.K_closest_elements.py
@@ -15,3 +15,27 @@ class Solution(object):
             heapq.heappop(lst)
                 
         return sorted([-el for _, el in lst])
+
+# Method 2
+
+
+class Solution(object):
+    def findClosestElements(self, arr, k, x):
+        """
+        :type arr: List[int]
+        :type k: int
+        :type x: int
+        :rtype: List[int]
+        """
+        n = len(arr)
+        left = 0
+        right = n - k
+
+        while left < right:
+            mid = left + (right-left)//2
+            if x - arr[mid] > arr[mid+k] - x:
+                left = mid + 1
+            else:
+                right = mid
+
+        return arr[right: right+k]
