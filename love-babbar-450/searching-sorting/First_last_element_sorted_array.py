a25df001ecee8260424a97398ccba8fc107fcda1
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-07-28 20:35:35 +0530

    added solution for DSA 450 sheet

--- love-babbar-450/searching-sorting/First_last_element_sorted_array.py
+++ love-babbar-450/searching-sorting/First_last_element_sorted_array.py
@@ -19,9 +19,39 @@ def firstLast(arr, target):
     return [left_idx if arr[left_idx] == target else -1, right_idx - 1 if right_idx > left_idx else -1]
 
 
+def f_bisect_left(arr, target):
+    left = 0
+    right = len(nums)
+
+    while left < right:
+        mid = left + (right-left) // 2
+        if nums[mid] < target:  # < condition based on notes below
+            left = mid + 1
+        else:
+            right = mid
+
+    return left
+
+
+def f_bisect_right(arr, target):
+    left = 0
+    right = len(nums)
+
+    while left < right:
+        mid = left + (right-left) // 2
+        if nums[mid] <= target:  # <= condition based on notes below
+            left = mid + 1
+        else:
+            right = mid
+
+    return right
+
+
 nums = [5, 7, 7, 8, 8, 10]
-target = 9
-print(firstLast(nums, target))
+target = 8
+# print(firstLast(nums, target))
+print(f_bisect_left(nums, target))
+print(f_bisect_right(nums, target))
 """
 # bisect_left :
   - for elements till arr[:i], e < x
