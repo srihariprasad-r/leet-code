4e276593271162d2e48a75f18f009fffc071ada1
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-08-20 11:47:00 +0530

    added solution for P659

--- leetcode-solutions/P0659.Split_array_consective_nums.py
+++ leetcode-solutions/P0659.Split_array_consective_nums.py
@@ -22,3 +22,31 @@ class Solution(object):
                 return False
 
         return True
+
+# Method 2
+class Solution(object):
+    def isPossible(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        cnt = collections.Counter(nums)
+        end = collections.Counter()
+        
+        for num in nums:
+            if cnt[num]:
+                cnt[num] -= 1
+                
+                # verify is previous number is available, change it to current number
+                if end[num-1]:
+                    end[num-1] -= 1
+                    end[num] += 1
+                # start new subsequence with current and check + 1/+2 are available
+                elif cnt[num+1] and cnt[num+2]:
+                    cnt[num+1] -= 1
+                    cnt[num+2] -= 1
+                    end[num+2] += 1
+                else:
+                    return False
+                
+        return True
