90665cf98dd6ae756fa3775d0f0b08772e3ce45b
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-09-07 22:09:49 +0530

    added solution for P907

--- leetcode-solutions/P0907.Sum_pf_subarray_minimum.py
+++ leetcode-solutions/P0907.Sum_pf_subarray_minimum.py
@@ -1,21 +1,19 @@
-# wrong submission
-
 class Solution(object):
     def sumSubarrayMins(self, arr):
         """
         :type arr: List[int]
         :rtype: int
         """
-        # arr.append(0)
+        arr.append(0)
         sum = 0
-        stck = []
-        
+        stck = [-1]
+
         for idx, el in enumerate(arr):
-            while stck  and el < arr[stck[-1]]:
+            while stck and el < arr[stck[-1]]:
                 idx1 = stck.pop()
                 left = idx1 - stck[-1]
                 right = idx - idx1
-                sum += left * arr[idx] * right
+                sum += left * arr[idx1] * right
             stck.append(idx)
-            
-        return sum
+
+        return sum % (10**9+7)
