d6c682ad547812460543aebf0e80e6e503a3752a
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-09-21 22:31:12 +0530

    added solution for P153

--- leetcode-solutions/P0153.Minimum_element_sorted_array.py
+++ leetcode-solutions/P0153.Minimum_element_sorted_array.py
@@ -17,3 +17,31 @@ class Solution(object):
                 right = mid
             
         return min(nums[left], nums[right])
+
+# Method 2
+
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        res = nums[0]
+        
+        left = 0
+        right = len(nums)- 1
+        
+        while left <= right:
+            if nums[left] < nums[right]:
+                res = min(res, nums[left])
+                break
+                
+            mid = left + (right - left) // 2
+            res = min(res, nums[mid])
+            
+            if nums[mid] >= nums[left]:
+                left = mid + 1
+            else:
+                right = mid - 1
+                
+        return res
