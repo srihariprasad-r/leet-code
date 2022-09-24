8743702f4df7dcf63dab17dd87a46dbc4237ee07
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-09-24 19:51:04 +0530

    added solution for P55

--- leetcode-solutions/P0055.Jump_Game.py
+++ leetcode-solutions/P0055.Jump_Game.py
@@ -11,3 +11,30 @@ class Solution(object):
                 tgt = i
 
         return True if tgt == 0 else False
+
+
+# TLE 
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        def dfs(nums, idx, nb):
+            if idx == len(nums)-1:
+                return True
+
+            next = idx + nums[idx]
+
+            for i in range(idx+1, next+1):
+                # if (i < len(nums) - 1 and nums[i] == 0):
+                #     return False
+                nb.add(i)
+                if i < len(nums):
+                    if dfs(nums, i, nb):
+                        return True
+                nb.remove(i)
+
+            return False
+
+        return dfs(nums, 0, set())
