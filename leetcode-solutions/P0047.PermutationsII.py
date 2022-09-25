4fc918b1f063cce01baf8851d5e0d5bd44fd8f99
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-09-25 19:14:48 +0530

    added solution for P47

--- leetcode-solutions/P0047.PermutationsII.py
+++ leetcode-solutions/P0047.PermutationsII.py
@@ -36,3 +36,49 @@ class Solution(object):
         
         return permutation(mp, nums, res, 0)
                 
+# Method 2 - TLE
+
+class Solution(object):
+    def permuteUnique(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        mp = {}
+        res = list()
+        fnl = list()
+        
+        nums.sort()
+
+        for n in nums:
+            if not(n in mp):
+                mp[n] = 1
+            else:
+                mp[n] += 1
+        
+
+        def permutation(mp, nums, res=[], lvl=0):
+            if lvl == len(nums):
+                if res not in fnl:
+                    fnl.append(res)
+                # res = []
+                return
+                
+            for i in range(len(nums)):
+                if not mp[nums[i]]:
+                    continue
+                
+                mp[nums[i]] -= 1
+                permutation(mp, nums, res + [nums[i]], lvl+1)
+                mp[nums[i]] += 1
+                
+            return fnl
+        
+        # return permutation(mp, nums, res, 0)
+    
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+                
+            return permutation(mp, nums, res, i)
+                
