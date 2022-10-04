7282ea2e5f27893aee2b3b982f5dbeb1b008d78f
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-10-04 17:33:16 +0530

    added solution for P62

--- leetcode-solutions/P0062.Uniquepath.py
+++ leetcode-solutions/P0062.Uniquepath.py
@@ -15,3 +15,24 @@ class Solution(object):
                     dp[i][j] = dp[i][j-1] +  dp[i-1][j]
                 
         return dp[-1][-1]
+
+
+# Method 2 - TLE
+
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        def dfs(i, j, cnt):
+            if i == m -1  and j == n - 1:
+                return 1
+            
+            if i > m -1 or j > n - 1:
+                return 0
+            
+            return dfs(i, j+1, cnt + 1) + dfs(i+1, j, cnt + 1)
+        
+        return dfs(0,0,0)
