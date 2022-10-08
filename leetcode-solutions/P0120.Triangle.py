9395454847a5f1e24cf2320268b181662c41df69
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-10-08 23:31:03 +0530

    added solution for P120

---
+++ leetcode-solutions/P0120.Triangle.py
@@ -0,0 +1,26 @@
+# TLE
+
+class Solution(object):
+    def minimumTotal(self, triangle):
+        """
+        :type triangle: List[List[int]]
+        :rtype: int
+        """
+        def recursion(i, j, m, n, triangle, dp):
+            if i == m - 1:
+                return triangle[i][j]
+            
+            if dp[i][j] > 0: return dp[i][j]
+            
+            down = triangle[i][j] + recursion(i+1, j, m, n, triangle, dp)
+            right = triangle[i][j] + recursion(i+1, j + 1, m, n, triangle, dp)
+            
+            dp[i][j] = min(down, right)
+            
+            return dp[i][j]
+        
+        m = len(triangle)
+                     
+        dp = [[0 for _ in range(m)] for _ in range(m)]
+        
+        return recursion(0, 0, m, m,  triangle, dp)
