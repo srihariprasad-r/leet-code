75fb3162043a7caff618c45797d4ae784548201b
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-10-10 23:00:44 +0530

    added solution for P416

--- leetcode-solutions/P0416.Partial_equal_subset_sum.py
+++ leetcode-solutions/P0416.Partial_equal_subset_sum.py
@@ -18,3 +18,39 @@ class Solution:
         partial_sum = sum(nums)/2
 
         return dfs(0, partial_sum)
+
+# Method 2
+
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        n = len(nums)
+        
+        if sum(nums) % 2 :
+            return False
+        
+        def recursion(idx, target, dp):
+            if idx == 0:
+                if target == 0:
+                    return True
+                else:
+                    return False
+                
+            if target == 0:
+                return True
+            
+            if nums[idx] == target:
+                return True
+            
+            if dp[idx][target] != -1 : return dp[idx][target]
+            
+            not_take = recursion(idx-1, target, dp)
+            take = recursion(idx-1, target - nums[idx], dp)
+            
+            dp[idx][target] = take or not_take
+            
+            return dp[idx][target]
+        
+        target = sum(nums)//2
+        dp = [[-1 for _ in range(target+1)] for _ in range(n+1) ]
+        
+        return recursion(n-1,target, dp)
