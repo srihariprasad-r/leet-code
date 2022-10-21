c3101114aa061ee30f2f0b9d32c771e607b38a87
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-10-21 22:42:40 +0530

    added solution for P718

--- leetcode-solutions/P0718.Max_length_equal_subarray.py
+++ leetcode-solutions/P0718.Max_length_equal_subarray.py
@@ -5,17 +5,58 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: int
         """
+        """
+        [1, 2, 3]
+        cur = 0
+        1   -> cur = cur*base + num = 1
+        12  -> cur = cur*base + num = 12
+        123 -> cur = cur*base + num = 123
+        
+        23 = 123 - 100 = cur - base ** (2 - 0)
+        """
+
+        def f(idx):
+            hash1 = set()
+            hash2 = set()
+            base = 101
+            mod = 2**32
+
+            cur = 0
+            for i in range(m):
+                cur = cur * base + nums1[i]
+
+                if i >= idx:
+                    cur -= nums1[i-idx] * base ** idx
+
+                cur %= mod
+                if i >= mid - 1:
+                    hash1.add(cur)
+
+            cur = 0
+            for i in range(n):
+                cur = cur * base + nums2[i]
+
+                if i >= idx:
+                    cur -= nums2[i-idx] * base ** idx
+
+                cur %= mod
+                if i >= mid - 1:
+                    hash2.add(cur)
+
+            return len(hash1.intersection(hash2)) > 0
+
         m = len(nums1)
         n = len(nums2)
-        ans = 0
 
-        dp = [[0]*(n+1) for _ in range(m+1)]
+        l = 0
+        r = min(m, n)
 
-        for i in range(1, m+1):
-            for j in range(1, n+1):
-                if nums1[i-1] == nums2[j-1]:
-                    dp[i][j] = 1 + dp[i-1][j-1]
+        while l < r:
+            mid = l + (r - l+1)//2
 
-                ans = max(ans, dp[i][j])
+            if f(mid):
+                l = mid
+            else:
+                r = mid - 1
 
-        return ans
+        return l
