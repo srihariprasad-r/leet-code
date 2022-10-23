ff31813bfd1f892ce9924ff4c389338370f0c0fe
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-10-23 19:56:59 +0530

    added solution for P34

--- leetcode-solutions/P0034.First_last_element_array.py
+++ leetcode-solutions/P0034.First_last_element_array.py
@@ -36,3 +36,44 @@ class Solution(object):
         if lt == len(nums) or nums[lt] != target:
             return [-1, -1]
         return [lt, searchright(nums)-1]
+
+# Method 2
+
+class Solution(object):
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        if len(nums) == 0:
+            return [-1, -1]
+
+        def searchleft(nums):
+            left = 0
+            right = len(nums)
+            while left < right:
+                mid = left + (right-left)//2
+                if nums[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid
+
+            return left
+
+        def searchright(nums):
+            left = 0
+            right = len(nums)
+            while left < right:
+                mid = left + (right-left)//2
+                if nums[mid] > target:
+                    right = mid
+                else:
+                    left = mid + 1
+
+            return right
+
+        lt = searchleft(nums)
+        if lt == len(nums) or nums[lt] != target:
+            return [-1, -1]
+        return [lt, searchright(nums)-1]
