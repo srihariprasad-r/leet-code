ce74af620509458bd2558986942935c6ed41a307
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-10-25 16:41:05 +0530

    added solution for P378

--- leetcode-solutions/P378.Kthsmallest_sorted_matrix.py
+++ leetcode-solutions/P378.Kthsmallest_sorted_matrix.py
@@ -9,3 +9,29 @@ class Solution(object):
         
         newlist.sort()
         return newlist[k-1]
+
+# Method 2
+
+class Solution(object):
+    def kthSmallest(self, matrix, k):
+        """
+        :type matrix: List[List[int]]
+        :type k: int
+        :rtype: int
+        """
+        n = len(matrix)
+        
+        pq = []
+        
+        for i in range(n):
+            heapq.heappush(pq, (matrix[0][i], 0, i))
+        
+        while k > 1:
+            nums, row, col = heapq.heappop(pq)
+
+            if row < n - 1:
+                heapq.heappush(pq, (matrix[row+1][col], row+1, col))
+                
+            k -= 1
+            
+        return pq[0][0]
