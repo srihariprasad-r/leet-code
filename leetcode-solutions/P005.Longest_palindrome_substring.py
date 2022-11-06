c23f7a5afcc062ba64a4a440406676ca382d6e27
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-11-06 19:42:59 +0530

    added solution for P5

--- leetcode-solutions/P005.Longest_palindrome_substring.py
+++ leetcode-solutions/P005.Longest_palindrome_substring.py
@@ -33,3 +33,33 @@ class Solution(object):
                 right = r
         
         return s[left: right +1]                        
+
+# Method 2 - move outwards
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        res = ""
+        ln = 0
+
+        for i in range(len(s)):
+            # odd length - "bab"
+            l, r = i, i
+
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if r - l + 1 > ln:
+                    ln = r - l + 1
+                    res = s[l:r+1]
+                l -= 1
+                r += 1
+
+            # even length: "cbbd"
+            l, r = i, i + 1
+
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if r - l + 1 > ln:
+                    ln = r - l + 1
+                    res = s[l:r+1]
+                l -= 1
+                r += 1
+
+        return res
