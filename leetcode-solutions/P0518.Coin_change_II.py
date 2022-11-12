bcd7bce0e904e88218e19f99712034ab5faa0b88
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-11-12 16:38:37 +0530

    added solution for P518

---
+++ leetcode-solutions/P0518.Coin_change_II.py
@@ -0,0 +1,21 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        if len(coins) == 0 or amount <= 0:
+            return 1
+
+        dp = [[-1 for _ in range(amount+1)] for _ in range(len(coins))]
+
+        for i in range(len(coins)):
+            dp[i][0] = 1
+
+        for i in range(len(coins)):
+            include = 0
+            exclude = 0
+            for j in range(1, amount+1):
+                if j >= coins[i]:
+                    include = dp[i][j-coins[i]]
+                if i > 0:
+                    exclude = dp[i-1][j]
+                dp[i][j] = include + exclude
+
+        return dp[-1][-1] if dp[-1][-1] > -1 else 0
