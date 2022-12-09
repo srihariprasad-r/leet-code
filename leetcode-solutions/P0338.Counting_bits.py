987144f82be3640e1e7f45c6c0571b095b1aa276
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-12-09 22:39:20 +0530

    added solution for P338

--- leetcode-solutions/P0338.Counting_bits.py
+++ leetcode-solutions/P0338.Counting_bits.py
@@ -5,3 +5,17 @@ class Solution:
             ans.append(bin(i)[2:].count('1'))
 
         return ans
+
+# Method 2 - DP
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        dp = [-1] * (n+1)
+        dp[0] = 0
+        r = 1
+        for i in range(1, n+1):
+            if r * 2 == i:
+                r *= 2
+            dp[i] = dp[i-r] + 1
+
+        return dp
