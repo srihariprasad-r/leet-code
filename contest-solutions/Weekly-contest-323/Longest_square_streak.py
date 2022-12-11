f253ee78c1aab0c8b0e7992276a1ae2f0061cfa2
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-12-11 18:21:47 +0530

    added solution for weekly contest 323

--- contest-solutions/Weekly-contest-323/Longest_square_streak.py
+++ contest-solutions/Weekly-contest-323/Longest_square_streak.py
@@ -18,3 +18,17 @@ class Solution:
             return max(take, no_take)
 
         return dfs(0,nums[0]* nums[0], 0)
+
+
+# Algorithms Casts
+
+class Solution:
+    def longestSquareStreak(self, nums: List[int]) -> int:
+        nums.sort(reverse=True)
+
+        dp = collections.defaultdict(int)
+
+        for num in range(len(nums)):
+            dp[nums[num]] = dp[nums[num] * nums[num]] + 1
+
+        return -1 if max(v for v in dp.values()) < 2 else max(v for v in dp.values())
