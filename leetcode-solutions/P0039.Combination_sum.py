64b4d5065aa9742b85dadf3623a3aff89bc3f69b
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-12-12 23:12:39 +0530

    added solution for P39

--- leetcode-solutions/P0039.Combination_sum.py
+++ leetcode-solutions/P0039.Combination_sum.py
@@ -21,3 +21,22 @@ class Solution(object):
             return res
 
         return dfs([], 0, 0)
+
+# Method 2 
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        def dfs(idx, s, arr, res):
+            if s == target:
+                res.append(copy.deepcopy(arr))
+                return
+
+            if idx >= len(candidates) or s > target:
+                return
+
+            take = dfs(idx, s + candidates[idx], arr + [candidates[idx]], res)
+            no_take = dfs(idx + 1, s, arr, res)
+
+            return res
+
+        return dfs(0, 0, [], [])
