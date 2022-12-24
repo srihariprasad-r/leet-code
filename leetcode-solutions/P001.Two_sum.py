1079f8540fd391a4ebdce3fecb124d2e155d039f
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-12-24 20:13:43 +0530

    added solution for P1

--- leetcode-solutions/P001.Two_sum.py
+++ leetcode-solutions/P001.Two_sum.py
@@ -10,3 +10,19 @@ class Solution(object):
             for j in range(i+1, len(nums)):                
                 if nums[i] ==  target - nums[j]:
                     return [i,j]
+
+# Method 2
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        mp = {}
+
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if not(diff in mp):
+                mp[nums[i]] = i
+            else:
+                ans= [mp[diff], i]
+
+                return ans
+            
