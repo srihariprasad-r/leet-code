0d42576bf50535926fe13b0fb49ed4411c1c14f0
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-12-25 18:26:44 +0530

    added solution for P347

--- leetcode-solutions/P0347.TopKfrequentelements.py
+++ leetcode-solutions/P0347.TopKfrequentelements.py
@@ -13,3 +13,23 @@ class Solution(object):
             res.append(heapq.heappop(heaplist)[1])
         
         return res
+
+# Method 2 - slightly different
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        mp = Counter(nums)
+        arr = []
+        res = []
+        for m, v in mp.items():
+            arr.append((-v, m))
+
+        heapq.heapify(arr)
+        j = 0
+        while j < k:
+            el = heapq.heappop(arr)
+            res.append(el[1])
+            j += 1
+
+        return res
