ae025e94c0bbd45aa1150dc0d1731d6c9af6ebba
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-12-25 19:03:48 +0530

    added solution for P238

--- leetcode-solutions/P0238.Product_except_self.py
+++ leetcode-solutions/P0238.Product_except_self.py
@@ -18,3 +18,22 @@ class Solution(object):
             ans.append(prefix[i] * suffix[i+1])
 
         return ans
+
+# Method 2 - slight variation
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        ans = []
+        prefix = [1] * len(nums)
+        suffix = [1] * len(nums)
+
+        for i in range(1, len(nums)):
+            prefix[i] = prefix[i-1] * nums[i-1]
+
+        for i in range(len(nums)-1, 0, -1):
+            suffix[i-1] = suffix[i] * nums[i]
+
+        for i in range(len(nums)):
+            ans.append(prefix[i] * suffix[i])
+
+        return ans
