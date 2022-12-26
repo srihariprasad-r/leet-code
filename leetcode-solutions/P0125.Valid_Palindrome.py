9bfcf08afcb4e110aa02a0e896769cc3e39f8196
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-12-26 20:13:35 +0530

    added solution for P125

--- leetcode-solutions/P0125.Valid_Palindrome.py
+++ leetcode-solutions/P0125.Valid_Palindrome.py
@@ -19,3 +19,17 @@ class Solution(object):
             p1 += 1
             p2 -= 1
         return True        
+
+# Method 2
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = re.sub(r'\W+', '', s).replace('_', '').lower()
+        i = 0
+        j = len(s) - 1
+        while i < j:
+            if s[i] != s[j]:
+                return False
+            i += 1
+            j -= 1
+        return True
