5c8e911b8e0532b618bcb331a7f0b8030888766c
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-12-28 18:39:10 +0530

    added solution for P20

--- leetcode-solutions/P020.Valid_Paranthesis.py
+++ leetcode-solutions/P020.Valid_Paranthesis.py
@@ -19,3 +19,25 @@ class Solution(object):
                 stack_list.pop()             
     
         return not stack_list
+
+# Method 2:
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) < 2 : return False
+
+        stck = []
+
+        for c in s:
+            if c in ('(', '{','['):
+                stck.append(c)
+            else:
+                if stck:
+                    el = stck.pop()
+                    if not((c == ')' and el == '(') or (c == '}' and el == '{') \
+                        or (c == ']' and el == '[')):
+                        return False
+                else:
+                    return False
+
+        return True if not stck else False
