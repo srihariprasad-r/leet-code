9accdd1dc06f556ccdb713215cdfeb7d4b76ddac
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2022-12-29 18:14:35 +0530

    added solution for P118

--- leetcode-solutions/P0118.Pascals_Triangle.py
+++ leetcode-solutions/P0118.Pascals_Triangle.py
@@ -24,3 +24,16 @@ class Solution(object):
             pascals.append(currentrow)        
         
         return pascals
+
+
+# Method 2
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        arr = [[1]*(i+1) for i in range(numRows)]
+
+        for i in range(2, numRows):
+            for j in range(1, i):
+                arr[i][j] = arr[i-1][j-1] + arr[i-1][j]
+
+        return arr
