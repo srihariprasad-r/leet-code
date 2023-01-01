81f3911a5ae44d0306988ae440e3b36afb0cb3ef
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-01-01 23:13:29 +0530

    added solution for P122

--- leetcode-solutions/P0122.Buy_Sell_stock_II.py
+++ leetcode-solutions/P0122.Buy_Sell_stock_II.py
@@ -44,3 +44,14 @@ class Solution(object):
             dp[i][1] = max(dp[i-1][1], dp[i-1][0] - prices[i])
 
         return dp[-1][0]
+
+# Method 3
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i-1]:
+                profit += prices[i] - prices[i-1]
+
+        return profit 
