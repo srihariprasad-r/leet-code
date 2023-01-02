8ce69175243bb00318ff6934e8e6864fd7687a69
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-01-02 22:21:56 +0530

    added solution for P121

--- leetcode-solutions/P0121.Buy_and_sell_stock.py
+++ leetcode-solutions/P0121.Buy_and_sell_stock.py
@@ -31,3 +31,18 @@ class Solution(object):
             max_profit = max(max_profit, selling_price - prices[i])
 
         return max_profit
+
+# Method 3
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        dp = [[0]* 2 for _ in range(len(prices))]
+
+        dp[0][0] = 0
+        dp[0][1] = -prices[0]
+
+        for i in range(1, len(prices)):
+            dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i])
+            dp[i][1] = max(dp[i-1][1], -prices[i])    # removed dp[i-1][0] as it was incorrect submission
+
+        return dp[-1][0]
