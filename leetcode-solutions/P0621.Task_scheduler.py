60d167592f21b7d3fe69ed0b1452f7a6131631d8
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-01-09 22:31:26 +0530

    added solution for P621

--- leetcode-solutions/P0621.Task_scheduler.py
+++ leetcode-solutions/P0621.Task_scheduler.py
@@ -21,3 +21,29 @@ class Solution(object):
             idle_space -= min(sorted_list.pop(), mx_cnt - 1)
 
         return idle_space + len(tasks)
+
+# Method 2
+
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        mp = Counter(tasks)
+
+        lst = []
+
+        for v in mp.values():
+            lst.append(-v)
+
+        t = 0
+        heapq.heapify(lst)
+        q = deque()
+
+        while lst or q:
+            t += 1
+            if lst:
+                el = heapq.heappop(lst)
+                c = 1 + el
+                if c: q.append([c, t + n])
+            if q and q[0][1] == t:
+                heapq.heappush(lst, q.popleft()[0])
+
+        return t
