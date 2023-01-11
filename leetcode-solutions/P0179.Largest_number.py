d78ea91a6ed06038e2555cfec57c796acaa7d5a8
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-01-11 23:23:14 +0530

    added solution for P179

--- leetcode-solutions/P0179.Largest_number.py
+++ leetcode-solutions/P0179.Largest_number.py
@@ -28,3 +28,44 @@ class Solution:
                 mx, int(''.join(list(map(lambda x: str(x), permutations[i])))))
 
         return str(mx)
+
+# Method 2
+
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        n = len(nums)
+        def mergesort(nums, l, r):
+            if l > r: return []
+            if l == r: return [nums[l]]
+
+            mid = l + (r-l) // 2
+            left = mergesort(nums, l, mid)
+            right = mergesort(nums, mid+1, r)
+
+            return merge(left, right)
+
+        def merge(l, r):
+            res = []
+            i = 0
+            j = 0
+
+            while i < len(l) and j < len(r):
+                if str(l[i]) + str(r[j]) > str(r[j]) + str(l[i]):
+                    res.append(l[i])
+                    i += 1
+                else:
+                    res.append(r[j])
+                    j += 1
+
+            while i < len(l):
+                res.append(l[i])
+                i += 1
+
+            while j < len(r):
+                res.append(r[j])
+                j += 1
+
+            return res 
+
+        o = mergesort(nums, 0, n-1)
+        return str(int(''.join(map(str, o))))
