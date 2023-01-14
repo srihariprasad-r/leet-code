b7bbbd3e9290f97d11c1ba6bdae30204fbfd208a
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-01-14 23:06:03 +0530

    added solution for P523

--- leetcode-solutions/P0523.Continous_subarray_sum.py
+++ leetcode-solutions/P0523.Continous_subarray_sum.py
@@ -1,14 +1,15 @@
-# wrong submission
-
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
         mp = defaultdict(int)
         s = 0
+
+        mp[0] = -1
         for i in range(len(nums)):
             s += nums[i]
             if s % k in mp:
                 if i - mp[s % k] > 1:
                     return True
-            mp[s % k] = i
+            else:
+                mp[s % k] = i
 
         return False
