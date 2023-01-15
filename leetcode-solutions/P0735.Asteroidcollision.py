9c61efe3d13202558fc754b12684a108425e65fb
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-01-15 17:11:27 +0530

    added solution for P735

--- leetcode-solutions/P0735.Asteroidcollision.py
+++ leetcode-solutions/P0735.Asteroidcollision.py
@@ -18,3 +18,25 @@ class Solution(object):
                     stck.pop()
           
         return stck
+
+# Method 2 - slight variation(wrong submission):
+
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stck = []
+
+        for i in range(len(asteroids)):
+            stck.append(asteroids[i])
+            while len(stck) > 1 and ((stck[-2] > 0 and stck[-1] < 0) \
+                    or (stck[-1] > 0 and stck[-2] < 0)):
+                el = stck.pop()
+                if abs(stck[-1]) > abs(el):
+                    continue
+                else:
+                    if abs(el) == abs(stck[-1]): 
+                        stck.pop()
+                    else:
+                        stck.pop()
+                        stck.append(el)
+
+        return stck
