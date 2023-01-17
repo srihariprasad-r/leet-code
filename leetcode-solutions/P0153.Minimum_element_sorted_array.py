a29216f6a156a6a3c0f4dfb1183e4d705ba97d2b
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-01-17 23:52:49 +0530

    added solution for P153

--- leetcode-solutions/P0153.Minimum_element_sorted_array.py
+++ leetcode-solutions/P0153.Minimum_element_sorted_array.py
@@ -52,22 +52,22 @@ class Solution(object):
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         l = 0
-        r = len(nums)
+        r = len(nums) - 1
 
         if len(nums) == 1:
             return nums[0]
-        if nums[l] < nums[r-1]:
+        if nums[l] < nums[r]:
             return nums[l]
 
-        while l < r:
-            mid = (l + (r-l+1)//2) % len(nums)
+        while l <= r:
+            mid = l + (r-l)//2
 
             if mid > 0 and nums[mid-1] > nums[mid]:
                 return nums[mid]
 
-            if nums[mid] > nums[r % len(nums)]:
+            if nums[mid] > nums[r]:
                 l = mid + 1
             else:
-                r = mid
+                r = mid - 1
 
         return -1
