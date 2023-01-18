9cfafbb6cbf3e1c539e3a27b2799e704ea2c75c9
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-01-19 00:00:48 +0530

    added solution for P33

--- leetcode-solutions/P0033.Search_element_rotated_array.py
+++ leetcode-solutions/P0033.Search_element_rotated_array.py
@@ -59,6 +59,7 @@ class Solution(object):
 
 # Method 3 - wrong submission
 
+
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         l = 0
@@ -69,9 +70,15 @@ class Solution:
             if nums[mid] == target:
                 return mid
 
-            if nums[l] > target and nums[mid] > target:
-                l = mid + 1
+            if nums[mid] > target:
+                if nums[l] > target:
+                    l = mid + 1
+                else:
+                    r = mid - 1
             else:
-                r = mid - 1
+                if nums[r] < target:
+                    r = mid - 1
+                else:
+                    l = mid + 1
 
         return -1
