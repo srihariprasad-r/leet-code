be3ddf84de5053cf94bd083c98b4359743206330
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-01-24 00:00:52 +0530

    added solution for P402

---
+++ leetcode-solutions/P0402.Remove_K_digits.py
@@ -0,0 +1,20 @@
+# wrong submission
+
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+        stck = []
+        for n in num:
+            while stck and int(stck[-1]) > int(n) and k > 0:
+                stck.pop()
+                k -= 1
+            stck.append(n)
+
+        while stck and k > 0:
+            stck.pop()
+            k -= 1
+
+        idx = 0
+        while stck and idx < len(stck) and stck[idx] == '0':
+            idx += 1
+
+        return ''.join(stck[idx:]) if stck else '0'
