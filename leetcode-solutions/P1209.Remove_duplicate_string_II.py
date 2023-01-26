25c8aced0fe1ad35c10171a01aa67b222616c75b
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-01-26 18:45:03 +0530

    added solution for P1209

--- leetcode-solutions/P1209.Remove_duplicate_string_II.py
+++ leetcode-solutions/P1209.Remove_duplicate_string_II.py
@@ -1,3 +1,26 @@
+# TLE
+
+class Solution:
+    def removeDuplicates(self, s: str, k: int) -> str:
+        while True:
+            cnt = 1
+            removed = False
+            for i in range(1, len(s)):
+                if s[i] == s[i-1]:
+                    cnt += 1
+                else:
+                    cnt = 1
+                if cnt == k:
+                    s = s[:i-k+1] + s[i+1:]
+                    removed = True
+                    cnt = 1
+                    break
+            if not removed:
+                break
+        return s
+                
+# Method - 2
+
 class Solution:
     def removeDuplicates(self, s: str, k: int) -> str:
         stck = [[' ', 0]]
