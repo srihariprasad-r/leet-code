1186991aab66c311114b5409177603ae6b6dfa1d
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-01-29 19:54:45 +0530

    added solution for P456

--- leetcode-solutions/P0456.132Pattern.py
+++ leetcode-solutions/P0456.132Pattern.py
@@ -16,3 +16,21 @@ class Solution(object):
         
             stck.append(nums[i])      
         return False
+
+# Method 2 - slightly different to above
+
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+        stck = []
+        v = float('-inf')
+        for i in range(len(nums)-1, -1, -1):
+            if nums[i] < v: 
+                return True
+
+            while stck and stck[-1] < nums[i]:
+                v = stck[-1]
+                stck.pop()
+
+            stck.append(nums[i])
+
+        return False
