6c72a37819db0c2f0373ed3d09bf157c0944a9d2
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-02-02 23:56:18 +0530

    added solution for P239

--- leetcode-solutions/P0239.Sliding_window_maximum.py
+++ leetcode-solutions/P0239.Sliding_window_maximum.py
@@ -19,3 +19,24 @@ class Solution(object):
                 ans.append(-pq[0][0])
 
         return ans
+
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        q = deque()
+
+        res = [-1] * (len(nums) - k + 1)
+
+        for i in range(len(nums)):
+            while q and q[0] <= i - k:
+                q.popleft()
+
+            while q and nums[q[-1]] < nums[i]:
+                q.pop()
+
+            q.append(i)
+
+            if i >= k - 1:
+                res[i-k+1] = nums[q[0]]
+
+        return res
