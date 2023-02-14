66978b7598bf70e0808a3289692cbe96fd8845d8
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-02-14 22:48:52 +0530

    added solution for P918

--- leetcode-solutions/P0918.Maximum_sum_circular_subarray.py
+++ leetcode-solutions/P0918.Maximum_sum_circular_subarray.py
@@ -14,3 +14,27 @@ class Solution:
             heapq.heappush(lst, (s, i))
 
         return ans
+
+# Method  2
+class Solution:
+    def maxSubarraySumCircular(self, nums: List[int]) -> int:
+        s = 0
+        ans = nums[0]
+
+        q = deque()
+        q.append((0, -1))
+        
+        for i in range(2*len(nums)):
+            s += nums[i%len(nums)]
+   
+            if q and i - q[0][1] > len(nums):
+                q.popleft()
+
+            ans = max(ans, s - q[0][0])
+
+            while q and q[-1][0] >= s:
+                q.pop()
+
+            q.append((s, i))
+
+        return ans
