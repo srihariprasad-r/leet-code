7ab0a0f2847eda458e76d6f6b9d6bb798bd1ac6d
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-02-22 23:35:47 +0530

    added solution for P2030

--- leetcode-solutions/P2030.Smallest_k_length_subsequence.py
+++ leetcode-solutions/P2030.Smallest_k_length_subsequence.py
@@ -1,21 +1,20 @@
-# wrong submission
-
 class Solution:
     def smallestSubsequence(self, s: str, k: int, letter: str, repetition: int) -> str:
         cnt = s.count(letter)
         stck = []
 
         for idx, c in enumerate(s):
-            while stck and stck[-1] > c and len(stck) + len(s) - idx > k and (stck[-1] != letter or
-                                                                              cnt > repetition):
+            while stck and ((stck[-1] > c and len(stck) + len(s) - idx > k \
+                and (stck[-1] != letter or \
+                cnt > repetition)) or k - len(stck) < repetition):
                 el = stck.pop()
                 if el == letter:
-                    repetition -= 1
+                    repetition += 1
 
             if len(stck) < k:
                 stck.append(c)
                 if c == letter:
-                    repetition += 1
+                    repetition -= 1
 
             if c == letter:
                 cnt -= 1
