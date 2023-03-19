3af31d7db2c345c637e0a0baae2d4f0954089820
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-03-19 19:55:35 +0530

    added solution for P912

---
+++ leetcode-solutions/P0912.Sort_array.py
@@ -0,0 +1,46 @@
+# TLE
+
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        if len(nums) <= 1: return nums
+        def merge(arr, l, mid, r):
+            tmp = [0] *  len(arr)
+
+            k = 0
+            low = l
+            high = mid + 1
+            while low <= mid and high <= r:
+                if arr[low] >= arr[high]:
+                    tmp[k] = arr[high]
+                    high += 1
+                    k += 1
+                else:
+                    tmp[k] = arr[low]
+                    k += 1
+                    low += 1
+
+            while low <= mid:
+                tmp[k] = arr[low]
+                k += 1
+                low += 1
+
+            while high <= r:
+                tmp[k] = arr[high]
+                k += 1   
+                high += 1     
+
+            for i in range(l, r+1):
+                arr[i] = tmp[i-l]
+
+            return arr
+        
+        def mergesort(arr, l, r):
+            if l == r: return
+            mid = (l + r)// 2
+            mergesort(arr, l, mid)
+            mergesort(arr, mid+1, r)
+            merge(arr, l, mid, r)
+
+            return arr
+
+        return mergesort(nums, 0, len(nums)-1)
