fe4feceee46e377cfb008057072476c4d26008de
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-03-20 21:01:13 +0530

    added solution for P493

--- leetcode-solutions/P0493.Reverse_pairs.py
+++ leetcode-solutions/P0493.Reverse_pairs.py
@@ -17,5 +17,58 @@ class Solution(object):
                 j += 1
             
         return cnt
+
+# merge sort - TLE
+class Solution:
+    def reversePairs(self, nums: List[int]) -> int:
+        def merge(arr, l, mid, r):
+            cnt = 0
+            low = l
+            high = mid + 1
+
+            for i in range(low, mid+1):
+                j = high
+                while j <= r and arr[i] > 2 * nums[j]:
+                    j += 1
+
+                cnt += j - (mid+1)
+            
+            tmp = [0] * (r-l+1)
+            k = 0
+            while low <= mid and high <= r:
+                if arr[low] >= arr[high]:
+                    tmp[k] = arr[high]
+                    high += 1
+                    k += 1
+                else:
+                    tmp[k] = arr[low]
+                    low += 1
+                    k += 1
+
+            while low <= mid:
+                tmp[k] = arr[low]
+                low += 1
+                k += 1
+
+            while high <= r:
+                tmp[k] = arr[high]
+                high += 1
+                k += 1
+
+            for i in range(l, r+1):
+                arr[i] = tmp[i-l]
+
+            return cnt
+
+        def mergesort(arr, l, r):
+            if l == r: return 0
+            mid = (l + r) //2
+            c = mergesort(arr, l, mid)
+            c += mergesort(arr, mid+1, r)
+            c += merge(arr, l, mid, r)
+
+            return c
+
+        return mergesort(nums, 0, len(nums)-1)
             
             
