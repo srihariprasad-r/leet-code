800a2eaaa27b3b78bdfcdfaf500fa0109b7a0432
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-03-28 23:41:25 +0530

    added solution for P28

--- leetcode-solutions/P028.First_occurance.py
+++ leetcode-solutions/P028.First_occurance.py
@@ -6,3 +6,39 @@ class Solution(object):
         :rtype: int
         """
         return haystack.find(needle)
+
+# KMP
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        arr = [0] * len(needle)
+        i = 1
+        j = 0
+
+        while i < len(needle):
+            if needle[i] == needle[j]:
+                arr[i] = j + 1
+                j += 1
+                i += 1
+            elif j == 0:
+                arr[i] = 0
+                i += 1
+            else:
+                j = arr[j-1]
+
+        i = 0
+        j = 0
+
+        while i < len(haystack):
+            if haystack[i] == needle[j]:
+                j += 1
+                i += 1
+            else:
+                if j == 0:
+                    i += 1
+                else:
+                    j = arr[j-1]
+
+            if j == len(needle):
+                return i - len(needle)
+
+        return -1
