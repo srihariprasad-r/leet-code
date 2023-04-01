a252a644cbe685f4dcd46b1a9ca1ac3dee4d71ff
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-04-01 18:01:55 +0530

    added solution for P567

--- leetcode-solutions/P0567.Permutations_in_string.py
+++ leetcode-solutions/P0567.Permutations_in_string.py
@@ -27,3 +27,34 @@ class Solution(object):
                 return True
        
         return False
+    
+# Method 2
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        cnt = Counter(s1)
+        w = defaultdict(int)
+        valid = 0
+
+        left = 0
+        right = 0
+
+        while right < len(s2):
+            c = s2[right]
+            if c in cnt:
+                w[c] += 1
+                if w[c] == cnt[c]:
+                    valid += 1
+
+            right += 1
+
+            while right - left >= len(s1):
+                if valid == len(cnt):
+                    return True
+                d = s2[left]
+                left += 1
+                if d in cnt:
+                    if w[d] == cnt[d]:
+                        valid -= 1
+                    w[d] -= 1
+
+        return False
