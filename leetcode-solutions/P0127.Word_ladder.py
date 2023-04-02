16a9d2ec77719a85238d2ef72722adf554074a9b
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-04-02 18:15:18 +0530

    added solution for P127

--- leetcode-solutions/P0127.Word_ladder.py
+++ leetcode-solutions/P0127.Word_ladder.py
@@ -29,3 +29,25 @@ class Solution:
             steps += 1
 
         return steps
+
+# TLE
+
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        if endWord not in wordList:
+            return 0
+
+        visited = set()
+        q = [(beginWord, 1)]
+
+        while q:
+            el, d = q.pop(0)
+            if el == endWord:
+                return d
+            for i in range(len(el)):
+                for c in 'abcdefghijklmnopqrstuvwxyz':
+                    newstr = el[:i] + c + el[i+1:]
+                    if newstr not in visited and newstr in wordList:
+                        visited.add(newstr)
+                        q.append((newstr, d + 1))
+
+        return 0
