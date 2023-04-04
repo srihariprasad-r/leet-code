631ee14673cfab1ebe3a9ac284df3a1d77c825e4
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-04-05 01:11:39 +0530

    added solution for P104

--- leetcode-solutions/P104.Maximum_Depth_Binary_Tree.py
+++ leetcode-solutions/P104.Maximum_Depth_Binary_Tree.py
@@ -17,3 +17,25 @@ class Solution(object):
             left_node = self.maxDepth(root.left)
             right_node = self.maxDepth(root.right)            
         return max(left_node, right_node) + 1
+
+
+# Method 2
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        self.depth = 0
+        self.res = float('-inf')
+
+        def traverse(node):
+            if not node:
+                return 0
+            self.depth += 1
+            if node.left:
+                traverse(node.left)
+            if node.right:
+                traverse(node.right)
+            self.res = max(self.res, self.depth)
+            self.depth -= 1
+            return self.res
+
+        return traverse(root)
