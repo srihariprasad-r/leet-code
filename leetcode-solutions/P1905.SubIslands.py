6197e201f382121a8c1c1294fd0ddfab4923e077
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-04-09 19:59:52 +0530

    added solution for P1905

--- leetcode-solutions/P1905.SubIslands.py
+++ leetcode-solutions/P1905.SubIslands.py
@@ -1,5 +1,3 @@
-# wrong submission
-
 class Solution:
     def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
         m = len(grid1)
@@ -14,15 +12,21 @@ class Solution:
                 return 0
 
             grid[i][j] = 0
-            dfs(grid, i , j - 1)
+            dfs(grid, i, j - 1)
             dfs(grid, i, j + 1)
             dfs(grid, i - 1, j)
             dfs(grid, i + 1, j)
 
+        # exclude islands in grid2 which are sea in grid1
         for i in range(m):
             for j in range(n):
-                if grid1[i][j] == 1 and grid2[i][j] == 1:
+                if grid1[i][j] == 0 and grid2[i][j] == 1:
+                    dfs(grid2, i, j)
+
+        for i in range(m):
+            for j in range(n):
+                if grid2[i][j] == 1:
+                    dfs(grid2, i, j)
                     res += 1
-                    dfs(grid1, i , j)
 
         return res
