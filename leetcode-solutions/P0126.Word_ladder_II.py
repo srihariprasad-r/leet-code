1976412783bef80e09bb98668ccb5b3cf2e9c224
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-04-22 01:45:38 +0530

    added solution for P126

--- leetcode-solutions/P0126.Word_ladder_II.py
+++ leetcode-solutions/P0126.Word_ladder_II.py
@@ -2,18 +2,20 @@
 
 class Solution:
     def findLadders(self, beginWord: str, endWord: str, wordList: List[str]) -> List[List[str]]:
-        q = deque([beginWord])
+        q = deque([(beginWord, beginWord)])
         seen = set()
-        res = []
-        ans = []
+        res = defaultdict(list)
+        ans = defaultdict(set)
+
         while q:
             n = len(q)
             for _ in range(n):
-                cur = q.popleft()
-                seen.add(cur)
+                parent, cur = q.popleft()
+                seen.add((parent, cur))
                 if cur == endWord:
-                    res.append(ans)
-                    ans = []
+                    ans[parent].add(cur)
+                    # ans = defaultdict(set)
+                    break
 
                 for i in range(len(cur)):
                     first = cur[:i]
@@ -21,9 +23,21 @@ class Solution:
                     t = ''
                     for x in 'abcdefghijklmnopqrstuvwxyz':
                         t = first + x + last
-                        if t not in seen and t in wordList:
-                            q.append(t)
-                            seen.add(t)
-                            ans.append(t)
- 
-        return res
+                        if (cur, t) not in seen and t in wordList:
+                            q.append((cur, t))
+                            seen.add((cur, t))
+                            if cur != parent and cur not in ans.keys():
+                                ans[parent].add(cur)
+
+        print(ans)
+
+        for k, v in ans.items():
+            st = list(v)
+            for i in range(len(st)):
+                res[k].append(st[i])
+                if st[i] in ans.keys():
+                    val = list(ans[st[i]])
+                    for j in range(len(val)):
+                        res[k].append(val[j])
+
+        print(res)
