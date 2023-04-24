69d4cf1f976f646dc3bde1cdcc2b132d08a60a9e
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-04-25 01:18:40 +0530

    added solution for P749

--- leetcode-solutions/P0749.Contains_virus.py
+++ leetcode-solutions/P0749.Contains_virus.py
@@ -2,7 +2,20 @@
 
 class Solution:
     def containVirus(self, isInfected: List[List[int]]) -> int:
-        res = float('-inf')
+        res = 0
+
+        def contaminate(grid, i, j):
+            if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]):
+                return
+
+            if grid[i][j] == 1: return
+
+            grid[i][j] = 1 
+            
+            contaminate(grid , i + 1, j)
+            contaminate(grid, i, j + 1)
+            contaminate(grid, i, j - 1)
+            contaminate(grid, i - 1, j)
 
         def dfs(grid, i, j):
             if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]):
@@ -10,10 +23,18 @@ class Solution:
 
             if grid[i][j] == 1: return 0
 
-            grid[i][j] = 1
+            grid[i][j] = 0
 
             return 1 + dfs(grid , i + 1, j) + dfs(grid, i, j + 1) \
                 + dfs(grid, i, j - 1) + dfs(grid, i - 1, j)
+
+        for i in range(len(isInfected)):
+            contaminate(isInfected, i, 0)
+            contaminate(isInfected, i, len(isInfected[0])-1)
+
+        for j in range(len(isInfected[0])):
+            contaminate(isInfected, 0, j)
+            contaminate(isInfected, len(isInfected)-1, j)
 
         for i in range(len(isInfected)):
             for j in range(len(isInfected[0])):
