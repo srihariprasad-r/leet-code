69a6d7749cfb09aa30b23f2e8ea1eddb14c6ceac
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-04-26 19:50:21 +0530

    added solution for P329

--- leetcode-solutions/P0329.Longest_increasing_path.py
+++ leetcode-solutions/P0329.Longest_increasing_path.py
@@ -32,3 +32,31 @@ class Solution:
                 res =  max(res, dfs(matrix, i, j))
 
         return res
+
+# Method 2
+
+
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        res = 0
+        dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        m = len(matrix)
+        n = len(matrix[0])
+        dp = {}
+
+        def dfs(grid, i, j):
+            if (i, j) in dp:
+                return dp[(i, j)]
+            res = 1
+            for d in dirs:
+                new_x, new_y = d[0] + i, d[1] + j
+                if 0 <= new_x < m and 0 <= new_y < n and grid[new_x][new_y] > grid[i][j]:
+                    res = max(res, 1 + dfs(grid, new_x, new_y))
+            dp[(i, j)] = res
+            return res
+
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                res = max(res, dfs(matrix, i, j))
+
+        return res
