baebf865f68e07a2f84ba8f151cbffcca8a02b12
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-04-26 19:32:19 +0530

    added solution for P329

--- leetcode-solutions/P0326.Longest_increasing_path.py
+++ leetcode-solutions/P0326.Longest_increasing_path.py
@@ -2,29 +2,33 @@
 
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
-        m = len(matrix)
-        n = len(matrix[0])
-        q = deque()
-        visited = set()
-        visited.add((0,0))
-        steps = 0
-        dirs = [(-1,0), (1, 0), (0, 1), (0, -1)]
-        q.append((0, 0))
+        res = 0
+        def dfs(grid, i, j):
+            m = len(grid)
+            n = len(grid[0])
+            q = deque()
+            steps = 0
+            dirs = [(-1,0), (1, 0), (0, 1), (0, -1)]
+            q.append((i, j))
 
-        while q:
-            t = len(q)
-            for _ in range(t):
-                c = q.popleft()
-                if c[0] >= m or c[1] >= n: 
-                    return steps
-                for d in dirs:
-                    new_x, new_y = c[0] + d[0], c[1] + d[1]
-                    if 0 <= new_x < m and 0 <= new_y < n and (new_x, new_y) not in visited:
-                        if matrix[c[0]][c[1]] < matrix[new_x][new_y]: 
-                            continue
-                        c = tuple([new_x, new_y])
-                        q.append(c)
-                        visited.add((new_x, new_y))
-            steps += 1
+            while q:
+                t = len(q)
+                steps += 1
+                for i in range(t):
+                    c = q.popleft()
+                    for d in dirs:
+                        new_x, new_y = c[0] + d[0], c[1] + d[1]
+                        if new_x >= 0 and new_x < m and new_y >= 0 and new_y < n:
+                            if grid[c[0]][c[1]] >= grid[new_x][new_y]: 
+                                continue
+                            c = tuple([new_x, new_y])
+                            q.append(c)
+                # steps += 1
 
-        return steps
+            return steps
+
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                res =  max(res, dfs(matrix, i, j))
+
+        return res
