d95e85f7fa7e012732287bb7f736cd663d6cf6f2
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-05-02 23:38:51 +0530

    added solution for P572

--- leetcode-solutions/P0572.Subtree.py
+++ leetcode-solutions/P0572.Subtree.py
@@ -1,6 +1,7 @@
 # Definition for a binary tree node.
 # wrong submission
 
+# Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
@@ -8,11 +9,16 @@
 #         self.right = right
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        def isSame(root1):
+            if root1 is None: return False
+
+            return dfs(root, subRoot) or dfs(root.left, subRoot) or dfs(root.right, subRoot)
+
         def dfs(node, subnode):
-            if node is None or subnode is None:
-                return True
+            if node is None or subnode is None: 
+                return node == subnode
 
             return node.val == subnode.val and \
                 dfs(node.left, subnode.left) and dfs(node.right, subnode.right)
 
-        return dfs(root, subRoot)
+        return isSame(root)
