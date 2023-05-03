7697998181fefccaa53cdeb8d1d99107f3d5adc7
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-05-03 23:55:31 +0530

    added solution for P687

--- leetcode-solutions/P0687.Longest_univalue_path.py
+++ leetcode-solutions/P0687.Longest_univalue_path.py
@@ -8,13 +8,16 @@
 #         self.right = right
 class Solution:
     def longestUnivaluePath(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
         def dfs(node, val, ln=0):
             if node is None:
                 return ln
 
-            return max(dfs(node.left, node.val if node.val != val else val,
-                           ln + 1 if node.val == val else ln),
-                       dfs(node.right, node.val if node.val != val else val,
-                           ln + 1 if node.val == val else ln))
+            val = node.val if node.val != val else val
+            ln = ln + 1 if node.val == val else ln
+            return max(dfs(node.left, val, ln),
+                       dfs(node.right, val, ln))
 
         return max(dfs(root.left, root.val, 0), dfs(root.right, root.val, 0))
