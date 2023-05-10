e80eded26daefb61fcc941c3db05653337f6f3b4
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-05-10 19:50:06 +0530

    added solution for P654

--- leetcode-solutions/P0654.Maximum_Binary_tree.py
+++ leetcode-solutions/P0654.Maximum_Binary_tree.py
@@ -19,3 +19,26 @@ class Solution(object):
             root.right = right
         
             return root
+
+# Method 2
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def constructMaximumBinaryTree(self, nums: List[int]) -> Optional[TreeNode]:
+        def dfs(nums):
+            if not nums: return
+
+            mx = max(nums)
+            i = nums.index(mx)
+            root = TreeNode(nums[i])
+            root.left = dfs(nums[:i])
+            root.right = dfs(nums[i+1:])
+
+            return root
+
+        return dfs(nums)
