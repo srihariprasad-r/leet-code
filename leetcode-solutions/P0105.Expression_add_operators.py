f952d880c63568a972cba9081254231b91c1e7b3
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-05-14 00:10:23 +0530

    added solution for P105

--- leetcode-solutions/P0105.Expression_add_operators.py
+++ leetcode-solutions/P0105.Expression_add_operators.py
@@ -1,24 +1,31 @@
-# wrong submission
+# TLE
 
 class Solution:
     def addOperators(self, num: str, target: int) -> List[str]:
-        res =[]
+        res = []
 
         def dfs(idx, ans):
-            if idx > len(num): return 
+            if idx > len(num):
+                return
 
             if idx == len(num):
                 t = ''.join(list(map(lambda x: ''.join(x), ans)))
 
-                if not t[-1].isnumeric(): t = t[:-1]
-                if eval(t) > target:
-                    return
+                if not t[-1].isnumeric():
+                    t = t[:-1]
+                try:
+                    if eval(t) > target:
+                        return
 
-                if int(eval(t)) == target:
-                    if t not in res: res.append(copy.deepcopy(t))
+                    if int(eval(t)) == target:
+                        if t not in res:
+                            res.append(copy.deepcopy(t))
+                        return
+                except:
                     return
             else:
                 for i in ('*', '-', '+'):
+                    dfs(idx+1, ans + [num[idx]])
                     tmp = [num[idx]]
                     if idx + 1 < len(num):
                         tmp += i
