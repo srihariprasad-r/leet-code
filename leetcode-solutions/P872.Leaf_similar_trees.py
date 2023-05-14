b2967325dff85faa153b7a9781ebd0acda744fbd
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-05-14 23:53:50 +0530

    added solution for P872

--- leetcode-solutions/P872.Leaf_similar_trees.py
+++ leetcode-solutions/P872.Leaf_similar_trees.py
@@ -58,3 +58,37 @@ class Solution(object):
 
             return ans
         return helper(root1) == helper(root2)
+
+# Method 3
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        n1 = list()
+        n2 = list()
+
+        def dfs(node1, fl='s'):
+            if not node1:
+                return None
+
+            if not node1.left and not node1.right:
+                if fl != 's':
+                    n1.append(node1.val)
+                else:
+                    n2.append(node1.val)
+                return
+
+            dfs(node1.left, fl)
+            dfs(node1.right, fl)
+
+            return
+
+        dfs(root1, fl='f')
+        dfs(root2)
+
+        return n1 == n2
