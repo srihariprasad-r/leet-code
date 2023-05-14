af0541c6addc0401586cd22e63728c5631829df1
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-05-14 19:47:51 +0530

    added solution for P617

--- leetcode-solutions/P617.Merge_two_Binary_trees.py
+++ leetcode-solutions/P617.Merge_two_Binary_trees.py
@@ -20,3 +20,41 @@ class Solution(object):
             t1.left = self.mergeTrees(t1.left, t2.left)
             t1.right = self.mergeTrees(t1.right, t2.right)
         return t1
+    
+# Method 2
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+        root = None
+
+        def dfs(node1, node2):
+            if not node1 and not node2:
+                return None
+            elif not node2 and node1:
+                return node1
+            elif node2 and not node1:
+                return node2
+
+            s = 0
+            if node1 and node2:
+                s = node1.val + node2.val
+            elif not node1 and node2:
+                s = node2.val
+            elif not node2 and node1:
+                s = node1.val
+
+            root = TreeNode(s)
+            root.left = dfs(node1.left, node2.left)
+            root.right = dfs(node1.right, node2.right)
+
+            return root
+
+        root = dfs(root1, root2)
+
+        return root
