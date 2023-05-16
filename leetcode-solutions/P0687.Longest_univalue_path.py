b701350c1b023fbc48efa4d8113d6b39c48652d8
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-05-17 01:07:06 +0530

    added solution for P687

--- leetcode-solutions/P0687.Longest_univalue_path.py
+++ leetcode-solutions/P0687.Longest_univalue_path.py
@@ -21,3 +21,35 @@ class Solution:
             return max(dfs(node.left, newval, ln), dfs(node.right, newval, ln))
 
         return dfs(root.left, root.val, 0) + dfs(root.right, root.val, 0)
+
+# Method 2
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def longestUnivaluePath(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        self.mx = 0
+
+        def dfs(node):
+            if node is None:
+                return 0
+
+            l = dfs(node.left)
+            r = dfs(node.right)
+
+            l = l + 1 if node.left and node.left.val == node.val else 0
+            r = r + 1 if node.right and node.right.val == node.val else 0
+
+            self.mx = max(self.mx, l + r)
+
+            return max(l, r)
+
+        dfs(root)
+
+        return self.mx
