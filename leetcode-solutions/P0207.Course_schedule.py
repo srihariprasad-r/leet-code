bc3be665936d7c15cace923e10efe17eb00aa0c9
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-05-27 20:14:59 +0530

    added solution for P207

--- leetcode-solutions/P0207.Course_schedule.py
+++ leetcode-solutions/P0207.Course_schedule.py
@@ -1,18 +1,20 @@
-# wrong submission
-
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         graph = collections.defaultdict(list)
-        self.course = True
+        visited = [False] * (numCourses)
+        self.course = False
 
         for edge in prerequisites:
-            graph[edge[1]].append(edge[0])
+            graph[edge[1]].append(edge[0])        
 
         def traverse(s, path=[]):
             if s in path:
-                self.course = False
-                return
+                self.course = True
+                return 
 
+            if visited[s]: return
+
+            visited[s] = True
             path.append(s)
             for v in graph[s]:
                 traverse(v, path)
@@ -22,4 +24,5 @@ class Solution:
 
         for i in range(numCourses):
             traverse(i)
-        return self.course
+
+        return not self.course
