3b1aed79df518a9837111a667391ff72aee17e4b
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-07-01 19:58:09 +0530

    updated P787

--- leetcode-solutions/P0787.Cheapest_flight.py
+++ leetcode-solutions/P0787.Cheapest_flight.py
@@ -3,27 +3,28 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
         dct = collections.defaultdict(list)
-
+        visited = [0] * (len(flights)+1)
         for s in flights:
             dct[s[0]].append((s[1], s[2]))
 
-        if src not in dct.keys():
-            return -1
+        if src not in dct.keys(): return -1
 
-        def dfs(src, dct, cost, ans, k):
-            if k < 0:
-                return float('inf')
+        def dfs(src, dct, cost, ans, k,visited):
+            if k < 0: return float('inf')
 
-            if src == dst:
-                ans = min(ans, cost)
+            if src == dst: 
+                ans = min(ans,cost)
                 return ans
 
             t = float('inf')
+            visited[src] = 1
 
             for x in dct[src]:
-                if cost + x[1] <= ans:
-                    t = min(t, dfs(x[0], dct, cost + x[1], ans, k - 1))
+                if not visited[x[0]] and cost + x[1] <= ans:
+                    t = min(t, dfs(x[0], dct, cost + x[1], ans , k - 1,visited))
+           
+                visited[src] = 0
 
-            return t
+            return t 
 
-        return dfs(src, dct, 0, float('inf'), k+1)
+        return dfs(src, dct, 0, float('inf') , k+1, visited)
