1a1fe4c703ba23ac53ee918c993b22c8b00acda5
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-07-01 18:58:24 +0530

    updated P787

--- leetcode-solutions/P0787.Cheapest_flight.py
+++ leetcode-solutions/P0787.Cheapest_flight.py
@@ -1,4 +1,4 @@
-# wrong submission
+# TLE
 
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
@@ -6,6 +6,9 @@ class Solution:
 
         for s in flights:
             dct[s[0]].append((s[1], s[2]))
+
+        if src not in dct.keys():
+            return -1
 
         def dfs(src, dct, cost, ans, k):
             if k < 0:
@@ -18,8 +21,9 @@ class Solution:
             t = float('inf')
 
             for x in dct[src]:
-                t = min(t, dfs(x[0], dct, cost + x[1], ans, k - 1))
+                if cost + x[1] <= ans:
+                    t = min(t, dfs(x[0], dct, cost + x[1], ans, k - 1))
 
             return t
 
-        return dfs(0, dct, s, float('inf'), k+1)
+        return dfs(src, dct, 0, float('inf'), k+1)
