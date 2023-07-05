8266c27f733839be7e66a1f7f07302ca759d4515
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-07-06 00:49:12 +0530

    updated solution for P210

--- leetcode-solutions/P0210.Course_schedule_II.py
+++ leetcode-solutions/P0210.Course_schedule_II.py
@@ -23,3 +23,50 @@ class Solution:
                         q.append(e)
 
         return ans if len(ans) == numCourses else []
+    
+# wrong submission
+
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        adj = collections.defaultdict(list)
+
+        for x in prerequisites:
+            adj[x[0]].append(x[1])
+
+        visited = [0] * numCourses 
+
+        def recursion(n):
+            if visited[n] == 2:
+                return True
+
+            visited[n] = 2
+            for i in range(len(adj[n])):
+                if visited[adj[n][i]] != 1:
+                    if recursion(adj[n][i]): return True
+
+            visited[n] = 1
+
+            return False
+
+        for i in range(numCourses):
+            if recursion(i):
+                return []
+
+        visited = [False] * numCourses 
+        stck = []
+
+        def tpsort(n):
+            visited[n] = True
+
+            for x in adj[n]:
+                if not visited[n]: tpsort(x)
+            
+            stck.append(n)
+
+            return 
+
+        for i in range(numCourses):
+            if not visited[i]:
+                tpsort(i)
+
+        return stck
