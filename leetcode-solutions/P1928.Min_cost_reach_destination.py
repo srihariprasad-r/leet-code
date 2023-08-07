fc4f1455bafd1496eb5469a509be8638b75155c1
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-08-07 19:53:36 +0530

    added solution for P1928

--- leetcode-solutions/P1928.Min_cost_reach_destination.py
+++ leetcode-solutions/P1928.Min_cost_reach_destination.py
@@ -23,3 +23,34 @@ class Solution:
         ans = dp(0, 0)
 
         return -1 if ans == float('inf') else ans
+
+# Method 2
+
+class Solution:
+    def minCost(self, maxTime: int, edges: List[List[int]], passingFees: List[int]) -> int:
+        graph = defaultdict(list)
+
+        n = len(passingFees)
+        for u, v, t in edges:
+            graph[u].append((v, t))
+            graph[v].append((u, t))
+
+        fees = [float('inf')] * n
+        times = [float('inf')] * n
+        fees[0] = passingFees[0]
+        times[0] = 0
+
+        q = [(passingFees[0], 0, 0)]
+
+        while q:
+            f, time, u = heapq.heappop(q)
+
+            for v, t in graph[u]:
+                if time + t <= maxTime and (f+passingFees[v] < fees[v] or time+t < times[v]):
+                    heapq.heappush(q, (f+passingFees[v], time+t, v))
+                    if f+passingFees[v] < fees[v]:
+                        fees[v] = f + passingFees[v]
+                    if time+t < times[v]:
+                        times[v] = time + t
+
+        return -1 if fees[-1] == float('inf') else fees[-1]
