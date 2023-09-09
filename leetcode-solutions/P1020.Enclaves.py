81fa9337b92b78d4700c3c5b58464f38b19b3fd1
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-09-09 23:48:21 +0530

    added solution for P1020

--- leetcode-solutions/P1020.Enclaves.py
+++ leetcode-solutions/P1020.Enclaves.py
@@ -32,3 +32,37 @@ class Solution:
                     res += 1
 
         return res
+
+# Method 2 - almost same
+class Solution:
+    def numEnclaves(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        res = 0
+
+        def dfs(i, j):
+            if i < 0 or i >= m or j < 0 or j >= n or grid[i][j] == 0:
+                return False
+
+            grid[i][j] = 0
+            dfs(i+1, j)
+            dfs(i, j + 1)
+            dfs(i-1, j)
+            dfs(i, j - 1)
+
+            return True
+
+        for i in range(m):
+            dfs(i,  0)
+            dfs(i, n-1)
+
+        for j in range(n):
+            dfs(0, j)
+            dfs(m-1, j)
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    res += 1
+
+        return res
