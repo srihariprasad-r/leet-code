6cdfe3101e5a831a925fd2c8ea6ccdfd474f1d8f
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-09-11 23:15:34 +0530

    added solution for P322

--- leetcode-solutions/P0322.Coin_change.py
+++ leetcode-solutions/P0322.Coin_change.py
@@ -73,3 +73,20 @@ class Solution:
         dp = [-1 for _ in range(amount+1)]
         dfs(0, amount, dp)
         return -1 if dp[-1] == float('inf') else dp[-1]
+    
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [[float('inf')] * (amount+1) for _ in range(len(coins)+1)]
+
+        for i in range(len(coins)+1):
+            dp[i][0] = 0
+
+        for i in range(1, len(coins)+1):
+            for j in range(1, amount+1):
+                if j - coins[i-1] < 0:
+                    dp[i][j] = dp[i-1][j]
+                else:
+                    dp[i][j] = min(dp[i][j-coins[i-1]] + 1, dp[i-1][j])
+
+        return dp[-1][-1] if dp[-1][-1] != float('inf') else -1
