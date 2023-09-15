dd57d8823726a8709a9f4095b4237c077695871a
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-09-15 19:32:12 +0530

    added solution for P118

--- leetcode-solutions/P0118.Pascals_Triangle.py
+++ leetcode-solutions/P0118.Pascals_Triangle.py
@@ -37,3 +37,15 @@ class Solution:
                 arr[i][j] = arr[i-1][j-1] + arr[i-1][j]
 
         return arr
+
+# Method 3 - almost same as #2
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        dp = [[1] * i for i in range(1,numRows+1)]
+        
+
+        for i in range(numRows):
+            for j in range(1, i):
+                dp[i][j] = dp[i-1][j] + dp[i-1][j-1]
+
+        return dp
