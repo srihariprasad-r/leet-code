8359337c72b33138eaeb494de7001b165c540204
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-09-24 00:24:17 +0530

    added solution for P392

--- leetcode-solutions/P392.Is_Subsequence.py
+++ leetcode-solutions/P392.Is_Subsequence.py
@@ -16,3 +16,23 @@ class Solution(object):
                     return True
             t_p += 1
         return False
+
+# Method 2
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        dp = [False] * len(s)
+        
+        def recurse(i, j):
+            if i < 0 or j < 0: return False
+
+            if s[i] == t[j]:
+                dp[i] = True
+                recurse(i-1, j-1)
+            else:
+                recurse(i, j - 1)
+
+            return True
+
+        recurse(len(s)-1, len(t)-1)
+        
+        return False if dp.count(True) != len(dp) else True
