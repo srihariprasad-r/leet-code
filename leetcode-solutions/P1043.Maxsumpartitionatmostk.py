c37904322d7f9ac52733a66811b977bb98525db0
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-10-01 20:06:34 +0530

    added solution for P1043

--- leetcode-solutions/P1043.Maxsumpartitionatmostk.py
+++ leetcode-solutions/P1043.Maxsumpartitionatmostk.py
@@ -16,3 +16,17 @@ class Solution(object):
                     dp[i] = max(dp[i], temp+(curmax*j))
     
         return dp[-1]
+
+# Method 2 - almost same
+class Solution:
+    def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
+        dp = [0] * len(arr)
+
+        for i in range(len(arr)):
+            mx = 0
+            for j in range(1, k+1):
+                if i - j + 1 >= 0:
+                    mx = max(mx, arr[i-j+1])
+                    dp[i] = max(dp[i], (dp[i-j] if i >= j else 0) + mx * j)
+
+        return dp[-1]
