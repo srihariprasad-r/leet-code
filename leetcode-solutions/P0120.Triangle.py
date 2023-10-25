581f8252aae05ef46419a457f5f826fae578cc02
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-10-26 00:15:28 +0530

    added solution for P120

--- leetcode-solutions/P0120.Triangle.py
+++ leetcode-solutions/P0120.Triangle.py
@@ -22,3 +22,13 @@ class Solution(object):
         dp = [[float('-inf') for _ in range(m)] for _ in range(m)]
         
         return recursion(0, 0, m, m,  triangle, dp)
+
+
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        for i in range(1, len(triangle)):
+            for j in range(i+1):
+                triangle[i][j] += min(triangle[i-1]
+                                      [j-(j == i)], triangle[i-1][j-(j > 0)])
+
+        return min(triangle[-1])
