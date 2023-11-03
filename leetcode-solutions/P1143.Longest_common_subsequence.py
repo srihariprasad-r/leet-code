4be16c8afa7f368dbb3196fd68b3ce02186e4071
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-11-03 22:35:16 +0530

    added solution for P1143

--- leetcode-solutions/P1143.Longest_common_subsequence.py
+++ leetcode-solutions/P1143.Longest_common_subsequence.py
@@ -15,3 +15,18 @@ class Solution(object):
                     dp[i][j] = max(dp[i-1][j], dp[i][j-1])
 
         return dp[len(text1)][len(text2)]
+
+# Method 2
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        dp = [[0 for _ in range(len(text1)+1)] for _ in range(len(text2)+1)]
+
+        for i in range(len(text2)-1, -1, -1):
+            for j in range(len(text1)-1, -1, -1):
+                if text2[i] == text1[j]:
+                    dp[i][j] = 1 + dp[i+1][j+1]
+                else:
+                    dp[i][j] = max(dp[i][j+1], dp[i+1][j])
+
+
+        return dp[0][0] 
