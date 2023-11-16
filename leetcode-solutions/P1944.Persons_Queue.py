0d96ab2c721b9fa4ddd0c1498e806fa51dce0484
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-11-16 16:40:52 +0530

    added solution for P1944

---
+++ leetcode-solutions/P1944.Persons_Queue.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canSeePersonsCount(self, heights: List[int]) -> List[int]:
+        stck = [0]
+        res = [0] * len(heights)
+
+        for i in range(1, len(heights)):
+            c = 0
+            while stck and heights[stck[-1]] < heights[i]:
+                j = stck.pop()
+                res[j] += 1
+            if stck:
+                res[stck[-1]] += 1
+            stck.append(i)
+
+        return res
