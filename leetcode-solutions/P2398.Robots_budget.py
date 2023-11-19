b6ac8f49239331a66f408e9d8231dc3a4f255580
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-11-19 23:41:55 +0530

    added solution for P2398

--- leetcode-solutions/P2398.Robots_budget.py
+++ leetcode-solutions/P2398.Robots_budget.py
@@ -1,4 +1,4 @@
-# TLE
+# wrong submission
 
 class Solution:
     def maximumRobots(self, chargeTimes: List[int], runningCosts: List[int], budget: int) -> int:
@@ -9,19 +9,19 @@ class Solution:
         res = float('-inf')
         stck = []
 
-        while j < len(chargeTimes):
+        while i <= j and j < len(chargeTimes):
             while stck and stck[-1] < chargeTimes[j]:
                 stck.pop()
             stck.append(chargeTimes[j])
-            
+
             rc += runningCosts[j] 
-            tc = stck[-1] + (j-i+1) *  rc
-            while tc > budget:
-                rc -= runningCosts[j] 
+            tc = stck[0] + (j-i+1) *  rc
+            while j >= i and tc > budget:
+                rc -= runningCosts[i] 
                 i += 1
-                tc = stck[-1] + (j-i+1) *  rc
+                tc = stck[0] + (j-i+1) *  rc
             
-            res = max(res, j- i + 1)
+            res = max(res, j- i+1)
 
             j += 1
 
