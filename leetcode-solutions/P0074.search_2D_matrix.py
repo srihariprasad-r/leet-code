09e0f279b4b8a9f21cfbe18af9da19749a358833
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-12-03 20:01:37 +0530

    added solution for P74

--- leetcode-solutions/P0074.search_2D_matrix.py
+++ leetcode-solutions/P0074.search_2D_matrix.py
@@ -61,3 +61,31 @@ class Solution(object):
                 return True
 
         return False
+
+#Method 3
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        def bs(arr, target):
+            l = 0
+            h = len(arr) - 1
+
+            while l <= h:
+                mid = l + (h-l)//2
+                if arr[mid] == target:
+                    return mid
+
+                if arr[mid] >= target:
+                    h = mid -  1
+                else:
+                    l = mid + 1
+            
+            return -1 
+
+        for i in range(len(matrix)):
+            idx = bs(matrix[i], target)
+            if  idx == -1:
+                continue
+            else:
+                return True
+
+        return False
