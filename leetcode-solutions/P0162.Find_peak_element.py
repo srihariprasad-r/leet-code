ff9a96f66fea2e756b9df06061a5e177e5855ab3
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-12-03 19:34:50 +0530

    added solution for P162

--- leetcode-solutions/P0162.Find_peak_element.py
+++ leetcode-solutions/P0162.Find_peak_element.py
@@ -16,3 +16,28 @@ class Solution(object):
                 right = mid
 
         return left
+
+# Method 2
+class Solution:
+    def findPeakElement(self, arr: List[int]) -> int:
+        n = len(arr)
+        if n == 1: return 0
+        # first element is peak
+        if arr[0] > arr[1] : return 0
+        # last element is peak
+        if arr[n-1] > arr[n-2]: return n-1
+        # rest all elements
+        l = 1
+        h = len(arr) - 2
+
+        while l <= h:
+            mid = l + (h-l) // 2
+            if arr[mid] > arr[mid-1] and arr[mid] > arr[mid+1]:
+                return mid
+            
+            if arr[mid] > arr[mid-1]:
+                l = mid + 1
+            else:
+                h = mid - 1
+        
+        return 
