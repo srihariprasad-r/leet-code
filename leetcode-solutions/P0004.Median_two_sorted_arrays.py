05652d77af14e363ee43d9198cdf34863788f7e3
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-12-09 00:08:31 +0530

    added solution for P4

--- leetcode-solutions/P0004.Median_two_sorted_arrays.py
+++ leetcode-solutions/P0004.Median_two_sorted_arrays.py
@@ -27,3 +27,33 @@ class Solution:
             s = arr[len(arr)//2]
 
         return s
+
+# Method 2
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        n1 = len(nums1)
+        n2 = len(nums2)
+        if n1 > n2: return self.findMedianSortedArrays(nums2, nums1) 
+        n = n1 + n2
+        low = 0 
+        high = n1
+        l = (n1+n2+1)//2
+        while low <= high:
+            mid = low + (high-low)//2
+            mid2 = l - mid
+            l1 = float('-inf')
+            l2 =  float('-inf')
+            r1 = float('inf')
+            r2 = float('inf')
+            if mid < n1: r1 = nums1[mid]
+            if mid2 < n2: r2 = nums2[mid2]
+            if mid - 1 >= 0: l1 = nums1[mid - 1]
+            if mid2 - 1 >= 0: l2 = nums2[mid2 - 1]
+            if l1 <= r2 and l2 <= r1:
+                if n % 2 == 1: 
+                    return max(l1, l2)
+                return (max(l1,l2)+ min(r1,r2)) / 2
+            elif l1 > r2:
+                high = mid - 1
+            else:
+                low = mid + 1
