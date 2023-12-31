fc0b7376953b08af4ce12bb792df6eee8ccb40ae
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-12-31 22:45:44 +0530

    added solution for P516

--- leetcode-solutions/P0516.Longestpalindromesubsequence.py
+++ leetcode-solutions/P0516.Longestpalindromesubsequence.py
@@ -95,3 +95,27 @@ class Solution:
                 mx = max(mx, len(o[i]))
 
         return mx
+
+# Memonization
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
+        s1 = s
+        s2 = s[::-1]
+
+        dp = [[-1 for _ in range(len(s1))] for _ in range(len(s1))]
+
+        def lcs(i, j):
+            if i < 0 or j < 0: return 0
+
+            if dp[i][j] > -1: return dp[i][j]
+            
+            c = 0
+            if s1[i] == s2[j]:
+                c = 1 + lcs(i-1, j-1)
+            else:
+                c += max(lcs(i, j-1), lcs(i-1,j))
+
+            dp[i][j] = c
+            return c
+        
+        return lcs(len(s1)-1 , len(s1)-1)
