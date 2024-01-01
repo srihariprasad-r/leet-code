0bfa3a1ec91f162bdb32d44228174ab8c45fa5de
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-01-01 22:33:37 +0530

    940

--- leetcode-solutions/P0940.Distint_subseq_II.py
+++ leetcode-solutions/P0940.Distint_subseq_II.py
@@ -2,14 +2,20 @@
 
 class Solution:
     def distinctSubseqII(self, s: str) -> int:
+        # s = ''.join(set(list(c for c in s)))
+        used = [0] * len(s)
         ans = []
         def subseq(idx, st):
             if st not in ans and len(st) > 0: ans.append(st)
 
             for i in range(idx, len(s)):
+                if used[i]: continue
+                if i > idx and s[i] == s[i-1] and not used[i-1]: continue
                 st += s[i]
+                used[i] = 1
                 subseq(i+1, st)
                 st = st[0:-1]
+                used[i] = 0
 
             return ans
 
