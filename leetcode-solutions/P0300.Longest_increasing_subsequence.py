463b9b78140f82762359a1d93a3b6dd34a45a39b
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-01-06 20:36:57 +0530

    P300

--- leetcode-solutions/P0300.Longest_increasing_subsequence.py
+++ leetcode-solutions/P0300.Longest_increasing_subsequence.py
@@ -17,3 +17,41 @@ class Solution(object):
             res = max(res, dp[i])
             
         return res
+
+# wrong submission
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        mx = float('-inf')
+        ans = list()
+        
+        def increasing(l):
+            if len(l) == 1: return True
+            i = 0
+            j = 1
+            if l[i] > l[j]: return False
+            while j < len(l):
+                if l[i] > l[j]:
+                    return False
+                i += 1
+                j += 1
+
+            return True
+            
+        def recurse(idx,arr):
+            if arr and arr not in ans: ans.append(arr[:])
+
+            for i in range(idx, len(nums)):
+                arr.append(nums[i])
+                recurse(i+1,arr)
+                arr.pop()
+                # recurse(idx+1, arr)
+
+            return ans
+
+        ls = recurse(0, [])
+        # print(ls)
+        for l in ls:
+            if increasing(l):
+                mx = max(mx, len(l))
+
+        return mx
