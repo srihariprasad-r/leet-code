e594883e0f36c960324cda5c136c671cf2d08aa5
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-01-15 18:45:49 +0530

    P207

--- leetcode-solutions/P0207.Course_schedule.py
+++ leetcode-solutions/P0207.Course_schedule.py
@@ -26,3 +26,29 @@ class Solution:
             traverse(i)
 
         return not self.course
+
+# wrong submission
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        lst  = collections.defaultdict(list)
+
+        visited = [0] * (numCourses+1)
+        for i in range(len(prerequisites)):
+            lst[prerequisites[i][0]].append(prerequisites[i][1])
+        
+        def dfs(node):
+            if visited[node] != 0: return False
+
+            visited[node] = 1
+            for n in lst[node]:
+                if not dfs(n):
+                    return False
+
+            return True
+
+        for i in range(len(prerequisites)):
+            for j in range(len(prerequisites[i])):
+                if not visited[prerequisites[i][j]] and not dfs(prerequisites[i][j]):
+                    return False
+
+        return True 
