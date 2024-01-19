989d2b737ee60ab57ca5cecaa0c317157c069708
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-01-19 23:31:58 +0530

    P130

---
+++ leetcode-solutions/P0130.Surrounded_regions.py
@@ -0,0 +1,35 @@
+# wrong submission
+
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        q = deque()
+        m = len(board)
+        n = len(board[0])
+        visited = [[0 for _ in range(n)] for _ in range(m)]
+        directions = [(-1, 0), (1,0),(0,1), (0,-1)]
+
+        for i in range(1, m-1):
+            for j in range(1, n-1):
+                if board[i][j] == 'O':
+                    q.append((i, j))
+                    visited[i][j] = 1
+
+        while q:
+            x, y = q.pop()
+
+            visited[x][y] = 1
+            surrounded = False
+
+            for d in directions:
+                newx = x + d[0]
+                newy =  y + d[1]
+                if board[newx][newy] == 'X':
+                    surrounded = True
+
+            if surrounded:
+                board[x][y] = 'X'
+
+        return board
