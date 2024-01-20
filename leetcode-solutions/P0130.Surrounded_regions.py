d6323c5e3119982ab0a50ca123b9f93d23d44afe
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-01-20 23:15:10 +0530

    P130

--- leetcode-solutions/P0130.Surrounded_regions.py
+++ leetcode-solutions/P0130.Surrounded_regions.py
@@ -11,29 +11,31 @@ class Solution:
         visited = [[0 for _ in range(n)] for _ in range(m)]
         directions = [(-1, 0), (1,0),(0,1), (0,-1)]
 
-        for i in range(1, m-1):
-            for j in range(1, n-1):
-                if board[i][j] == 'O':
-                    q.append((i, j))
-                    visited[i][j] = 1
+        for i in range(m):
+            for j in range(n):
+                if i == 0 or i == m-1 or j == 0 or j == n-1:
+                    if board[i][j] == 'O':
+                        q.append((i, j))
+                        visited[i][j] = 1
 
         while q:
             x, y = q.popleft()
 
             visited[x][y] = 1
-            surrounded = False
 
             for d in directions:
                 newx = x + d[0]
                 newy =  y + d[1]
-                if newx > 0 and newx < m -1 and newy > 0 and newy < n- 1 and \
-                    board[newx][newy] == 'X' and not visited[newx][newy]:
-                    surrounded = True
+                if newx > 0 and newx < m -1 and newy > 0 and newy < n- 1 \
+                    and not visited[newx][newy]:
+                    if board[newx][newy] == 'O': 
+                        board[newx][newy] = '#'
                     visited[newx][newy] = 1
-                # else:
-                #     surrounded = False
 
-            if surrounded:
-                board[x][y] = 'X'
 
+        for i in range(1, m-1):
+            for j in range(1,n-1):
+                if board[i][j] == 'O':
+                    board[i][j] = 'X'
+                    
         return board
