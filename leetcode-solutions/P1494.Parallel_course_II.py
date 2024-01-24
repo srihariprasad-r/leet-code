246cef28988e63ea3d79f12296f6bd58c87bec1b
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-01-24 23:07:25 +0530

    P1494

--- leetcode-solutions/P1494.Parallel_course_II.py
+++ leetcode-solutions/P1494.Parallel_course_II.py
@@ -2,8 +2,14 @@
 
 class Solution:
     def minNumberOfSemesters(self, n: int, relations: List[List[int]], k: int) -> int:
+        if not relations:
+            if n % k == 0:
+                return n // k
+            else: return n // k + 1
+
         lst = collections.defaultdict(list)
         q = deque()
+        v = 0
         ans = 0
         indegree = [0] * (n+1)
 
@@ -13,16 +19,20 @@ class Solution:
 
         for idx, val in enumerate(indegree):
             if indegree[idx] == 0:
-                q.append((val))
+                q.append(val)
 
         while q:
             course = q.popleft()
+            while v < k:
+                for c in lst[course]:
+                    indegree[c] -= 1
+                    if indegree[c] == 0:
+                        q.append(c)
 
-            for c in lst[course]:
-                indegree[c] -= 1
-                if indegree[c] == 0:
-                    q.append(c)
+                v += 1
 
+            v = 0
             ans += 1
+
 
         return ans
