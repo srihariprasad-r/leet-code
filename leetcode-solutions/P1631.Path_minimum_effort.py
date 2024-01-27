907cb21d851655a6544d2e499dbef1c7528787f3
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-01-27 20:11:07 +0530

    P1631

---
+++ leetcode-solutions/P1631.Path_minimum_effort.py
@@ -0,0 +1,29 @@
+# wrong submission
+
+class Solution:
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        directions = [(0, -1), (0, 1), (-1,0),(1,0)]
+        mx = float('inf')
+        visited = [[0 for _ in range(len(heights[0]))] for _ in range(len(heights))]
+
+        l =[(heights[0][0], (0, 0))]
+        heapq.heapify(l)
+        visited[0][0] = 1        
+        
+        while l:
+            dist, a = heapq.heappop(l)
+
+            if a[0] == len(heights)-1 and a[1] == len(heights[0])-1:
+                mx = min(mx, dist)
+
+            for d in directions:
+                newx = a[0] + d[0]
+                newy = a[1] + d[1]
+                if newx > -1 and newx < len(heights) \
+                    and newy > -1 and newy < len(heights[0]) \
+                    and visited[newx][newy] == 0:
+                    heapq.heappush(l, \
+                        (abs(heights[a[0]][a[1]] - heights[newx][newy]), (newx, newy)))
+                    visited[newx][newy] = 1
+
+        return mx
