cbd902ffe27d3e1646434d3f05776f04476962a8
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-01-28 19:32:05 +0530

    P1631

--- leetcode-solutions/P1631.Path_minimum_effort.py
+++ leetcode-solutions/P1631.Path_minimum_effort.py
@@ -5,18 +5,17 @@ class Solution:
         directions = [(0, -1), (0, 1), (-1,0),(1,0)]
         mx = float('inf')
         visited = [[0 for _ in range(len(heights[0]))] for _ in range(len(heights))]
+        distance = [[0 for _ in range(len(heights[0]))] for _ in range(len(heights))]
 
-        l =[(-heights[0][0],heights[0][0], (0, 0))]
+        l =[(heights[0][0], (0, 0))]
         heapq.heapify(l)
         visited[0][0] = 1        
         
         while l:
-            m, dist, a = heapq.heappop(l)
+            dist, a = heapq.heappop(l)
    
-            m = max(m, dist)
-
             if a[0] == len(heights)-1 and a[1] == len(heights[0])-1:
-                mx = min(mx, m)
+                return dist
 
             for d in directions:
                 newx = a[0] + d[0]
@@ -24,8 +23,10 @@ class Solution:
                 if newx > -1 and newx < len(heights) \
                     and newy > -1 and newy < len(heights[0]) \
                     and visited[newx][newy] == 0:
-                    heapq.heappush(l, \
-                        (-m, abs(heights[a[0]][a[1]] - heights[newx][newy]), (newx, newy)))
-                    visited[newx][newy] = 1
+                    newdist = max(abs(heights[a[0]][a[1]] - heights[newx][newy]), dist)
+                    if newdist < distance[newx][newy]:
+                        distance[newx][newy] = newdist
+                        heapq.heappush(l, (newdist, (newx, newy)))
+                        visited[newx][newy] = 1
 
-        return mx
+        return 0
