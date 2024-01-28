6f80b53971a5ac8e46488206bdac7430fff6c030
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-01-28 19:39:00 +0530

    P1631

--- leetcode-solutions/P1631.Path_minimum_effort.py
+++ leetcode-solutions/P1631.Path_minimum_effort.py
@@ -4,12 +4,12 @@ class Solution:
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
         directions = [(0, -1), (0, 1), (-1,0),(1,0)]
         mx = float('inf')
-        visited = [[0 for _ in range(len(heights[0]))] for _ in range(len(heights))]
-        distance = [[0 for _ in range(len(heights[0]))] for _ in range(len(heights))]
+        distance = [[float('inf') for _ in range(len(heights[0]))] \
+            for _ in range(len(heights))]
 
-        l =[(heights[0][0], (0, 0))]
+        l =[(0, (0, 0))]
         heapq.heapify(l)
-        visited[0][0] = 1        
+        distance[0][0] = 0       
         
         while l:
             dist, a = heapq.heappop(l)
@@ -21,12 +21,10 @@ class Solution:
                 newx = a[0] + d[0]
                 newy = a[1] + d[1]
                 if newx > -1 and newx < len(heights) \
-                    and newy > -1 and newy < len(heights[0]) \
-                    and visited[newx][newy] == 0:
+                    and newy > -1 and newy < len(heights[0]):
                     newdist = max(abs(heights[a[0]][a[1]] - heights[newx][newy]), dist)
                     if newdist < distance[newx][newy]:
                         distance[newx][newy] = newdist
                         heapq.heappush(l, (newdist, (newx, newy)))
-                        visited[newx][newy] = 1
 
         return 0
