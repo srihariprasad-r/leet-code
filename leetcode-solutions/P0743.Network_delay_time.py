f790bb4711ba0fc351fd8d70c50f3162ae115e67
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-01-30 18:34:18 +0530

    P743

--- leetcode-solutions/P0743.Network_delay_time.py
+++ leetcode-solutions/P0743.Network_delay_time.py
@@ -20,3 +20,26 @@ class Solution:
                     heapq.heappush(q, (w1 + w2, n2))
 
         return time if len(visited) == n else -1
+
+# wrong submission
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        lst = collections.defaultdict(list)
+        distance = [float('inf')]*(n+1)
+
+        for t in times:
+            lst[t[0]].append((t[1], t[2]))
+
+        q =  deque()
+        q.append((k, 0))
+        distance[k] = 0
+
+        while q:
+            node, d = q.popleft()
+
+            for l in lst[node]:
+                if l[1] + distance[node] < distance[l[0]]:
+                    distance[l[0]] = distance[node] + l[1]
+                    q.append((l[0], l[1]+distance[node]))
+
+        return -1 if distance[n] == float('inf') else distance[n]
