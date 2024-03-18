db87e26441e37e2f53d5b4b54c3a6125107a3589
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-03-18 20:29:01 +0530

    P1679

--- leetcode-solutions/P1679.Max_num_K_sum_pairs.py
+++ leetcode-solutions/P1679.Max_num_K_sum_pairs.py
@@ -3,40 +3,26 @@
 class Solution:
     def maxOperations(self, arr: List[int], k: int) -> int:
         import collections
-        from collections import deque
-        d = collections.defaultdict(list)
-        for i in range(len(arr)-1):
-            j = i + 1
-            s = arr[i]
-            while j < len(arr):
-                s += arr[j]
-                if s == k:
-                    d[s].append((i,j))
-                    
-                if s in d: 
-                    if (i,j) not in d[s]:
-                        d[s].append((i,j))
-                else:
-                    d[s].append((i,j))
-                
-                    
-                s -= arr[j]
-                j += 1
-                
-        q = deque()
-        
-        m, n = d[k][0]
-        
-        for i in range(1, len(d[k])):
-            q.append(d[k][i])
-        
-        cnt = 1
-        
-        while q:
-            x, y = q.popleft()
-            if x == m or y == n or x == n or y == m:
-                continue
+        d = collections.defaultdict(int)
+        used = collections.defaultdict(int)
+        visited = [0] * (max(arr)+1)
+        cnt = 0
+
+        for i in range(len(arr)):
+            s = k - arr[i]
+            if s in d:
+                d[s] += 1
             else:
-                cnt += 1
+                d[s] = 1
+
+        for i in range(len(arr)):
+            if arr[i] not in used:
+                if k-arr[i] not in used:
+                    if arr[i] == k-arr[i]:
+                        if d[arr[i]] > 1: cnt += 1
+                    else:
+                        cnt += min(d[arr[i]], d[k-arr[i]])
+                    used[arr[i]] = i
+                    used[k-arr[i]] = i
                 
         return cnt
