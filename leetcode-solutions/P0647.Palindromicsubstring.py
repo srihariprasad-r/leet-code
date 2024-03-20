e7b0576f38faf5528ea98b517a63f4c242397f8b
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-03-20 20:02:23 +0530

    P647

--- leetcode-solutions/P0647.Palindromicsubstring.py
+++ leetcode-solutions/P0647.Palindromicsubstring.py
@@ -52,3 +52,35 @@ class Solution:
                 r += 1
 
         return cnt
+
+# Method 3
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        dp = [[0 for _ in range(len(s))] for _ in range(len(s))]
+        cnt = 0
+
+        l = 1
+        for i in range(len(s)):
+            dp[i][i] = 1
+            cnt += 1
+
+
+        i = 0
+        while i < len(s) - 1:
+            if s[i] == s[i+1]:
+                dp[i][i+1] = 1
+                cnt += 1
+            i += 1
+
+        l = 3
+        while l <= len(s):
+            i = 0
+            while i < len(s) - l + 1:
+                j = i + l - 1
+                if s[i] == s[j] and dp[i+1][j-1] == 1:
+                    dp[i][j] =  1
+                    cnt += 1
+                i += 1
+            l += 1
+
+        return cnt
