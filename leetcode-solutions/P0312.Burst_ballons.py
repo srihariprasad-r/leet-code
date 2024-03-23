41973d6004d2940c5db3b576f001d5c358996494
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-03-23 22:50:53 +0530

    P312

--- leetcode-solutions/P0312.Burst_ballons.py
+++ leetcode-solutions/P0312.Burst_ballons.py
@@ -1,44 +1,24 @@
-# wrong submission
 class Solution:
     def maxCoins(self, arr: List[int]) -> int:
-        import copy 
-
-        visited = [0] * len(arr)
-        res = []
-        def permute(r):
-            if len(r) == len(arr):
-                res.append(copy.deepcopy(r))
-                return res
-                
-            for i in range(len(arr)):
-                if not visited[i]:
-                    visited[i]= 1
-                    r.append(i)
-                    permute(r)
-                    visited[i] = 0
-                    r.pop()
-                
-            return res
-    
-        orders = permute([])
-        mx = float('-inf')
-
-        for k, order in enumerate(orders):
-            m = 0
-            tmp = arr
-            bursted = [0] * len(arr)
-            for i in range(len(order)):
-                idx = order[i]
-                if idx == 0 :
-                    m += ((tmp[idx]) * (tmp[idx+1] if bursted[idx+1] != 1 else 1))
-                elif idx == len(arr)-1:
-                    m += ((tmp[idx-1] if bursted[idx-1] != 1 else 1) * (tmp[idx]))
-                else:
-                    before = tmp[idx-1] if bursted[idx-1] != 1 else 1
-                    at = tmp[idx] if bursted[idx] != 1 else 1
-                    nxt = tmp[idx+1] if bursted[idx+1] != 1 else 1
-                    m +=  (before * at  * nxt)
-                mx = max(mx, m)
-                bursted[idx] = 1
-                
-        return mx
+        dp = [[0 for _ in range(len(arr))] for _ in range(len(arr))]
+        for i in range(len(arr)):
+            dp[i][i] = ((arr[i]) \
+            * (arr[i-1] if i -1 > -1 else 1) * (arr[i+1] if i + 1 < len(arr) else 1))
+            
+        l = 2
+        while l <= len(arr):
+            i = 0
+            while i <= len(arr) - l:
+                j = i + l - 1
+                k = i
+                while k <= j:
+                    s = ((arr[i-1] if i-1>-1 else 1) \
+                    * (arr[k])* (arr[j+1] if j + 1 < len(arr) else 1))
+                    s += dp[i][k-1] if k - 1 >= i else 0
+                    s += dp[k+1][j] if k + 1 <= j else 0
+                    dp[i][j] = max(dp[i][j], s)
+                    k += 1
+                i += 1
+            l += 1
+            
+        return dp[0][len(arr)-1]
