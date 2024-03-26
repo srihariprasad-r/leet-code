9796919dc0041aa1e9dc4eee613ed814bad60230
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-03-26 19:01:14 +0530

    P2767

--- leetcode-solutions/P2767.Partition_strings_power_of_5.py
+++ leetcode-solutions/P2767.Partition_strings_power_of_5.py
@@ -9,12 +9,13 @@ class Solution:
         for i in range(1,len(s)):
             j = i
             t = s[j]
-            v = 0
-            while j > -1 :
-                if str(t) in powersOf5: 
-                    v = 1 + dp[j-1]
+            if s[i] == '0': continue
+            v = float('inf')
+            while j > -1:
+                if str(t) in powersOf5:  
+                    v = min(v, 1 + dp[j-1])
                 j -= 1
                 t += s[j]
             dp[i] =  v
         
-        return dp[len(s)-1] if dp[len(s)-1] > 0 else -1
+        return dp[len(s)-1] if dp[len(s)-1] != 0 else -1
