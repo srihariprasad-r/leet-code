1f8b106c7c025ae40c2858f2eba4c87a2f8549b6
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-04-08 23:49:42 +0530

    P19

--- leetcode-solutions/P0019.Remove_nth_node_linked_list.py
+++ leetcode-solutions/P0019.Remove_nth_node_linked_list.py
@@ -27,3 +27,33 @@ class Solution(object):
         second.next = second.next.next
         
         return head
+
+# Method 2 - wrong submission
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        if ((not head) or (head and not head.next and n == 1)): return None
+    
+        c = 1
+        tmp = head
+
+        while tmp.next:
+            tmp = tmp.next
+            c += 1
+
+        t = 0
+        prev = None
+        tmp = head
+        
+        while t < c - n :
+            prev = tmp
+            tmp = tmp.next
+            t += 1
+
+        prev.next = tmp.next
+
+        return head
