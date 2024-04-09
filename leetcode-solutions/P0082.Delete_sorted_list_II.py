aac2b5ec100fa5dd75a8dc36b4b29b7568027474
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-04-09 20:37:13 +0530

    P82

---
+++ leetcode-solutions/P0082.Delete_sorted_list_II.py
@@ -0,0 +1,34 @@
+# wrong submission
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head: return None
+        st = set()
+        tmp = head
+
+        while tmp.next:
+            if tmp.val == tmp.next.val:
+                st.add(tmp.val)
+                tmp = tmp.next.next
+            else:
+                tmp = tmp.next
+
+        dummy = ListNode(101)
+        dummy.next = head
+        tmp = dummy
+
+        while tmp.next:
+            if tmp.val in st:
+                while tmp.next and tmp.next.val in st:
+                    tmp.next = tmp.next.next
+            else:
+                if tmp.next.val in st:
+                    tmp.next = tmp.next.next
+                else:
+                    tmp = tmp.next
+
+        return dummy.next
