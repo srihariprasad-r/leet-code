0a03d6695a0c20bb713e910db961e70f0647f094
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-04-09 23:55:03 +0530

    P328

--- leetcode-solutions/P328.Odd_Even_linked_list.py
+++ leetcode-solutions/P328.Odd_Even_linked_list.py
@@ -25,3 +25,26 @@ class Solution(object):
         p1.next = p3
         
         return head
+
+# Method 2 - almost same
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head: return None
+        first = head
+        second = head.next
+        c = second
+
+        while first.next and second.next:
+            first.next = first.next.next
+            second.next = second.next.next
+            first = first.next
+            second = second.next
+
+        first.next = c
+
+        return head
