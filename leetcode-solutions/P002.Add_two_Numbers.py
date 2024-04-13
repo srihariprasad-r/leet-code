81fed8a39e1c3fdb11fe5bdd2fe5866bc9da6cd9
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-04-13 23:07:54 +0530

    P2

--- leetcode-solutions/P002.Add_two_Numbers.py
+++ leetcode-solutions/P002.Add_two_Numbers.py
@@ -38,3 +38,33 @@ class Solution(object):
             l3 = l3.next
         
         return ret.next
+    
+# Method 2 - wrong submission
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        c = 0
+        s = 0
+        c1 = l1
+        c2 = l2
+
+        dummy = ListNode(None)
+        new = dummy
+
+        while c1 and c2:
+            s += c1.val + c2.val
+            if c > 0: 
+                s += c
+                c = 0
+            c1 = c1.next
+            c2 = c2.next
+            s , c = s % 10, s // 10
+            new.next = ListNode(s)
+            new = new.next
+            s = 0
+
+        return dummy.next
