d369ad80c6bd71b54149006bd8384a87c917bbda
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-04-13 17:00:47 +0530

    P24

--- leetcode-solutions/P0024.Swap_pair_linkedlist.py
+++ leetcode-solutions/P0024.Swap_pair_linkedlist.py
@@ -24,3 +24,28 @@ class Solution(object):
             curr = nxt
             
         return dummy.next
+
+# Method 2 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head: return None
+        if not head.next: return head
+
+        dummy = ListNode(None)
+        dummy.next = head
+        cur = dummy
+
+        while cur.next and cur.next.next:
+            first = cur.next
+            sec = cur.next.next
+            cur.next = sec
+            first.next = sec.next
+            sec.next = first
+            cur = cur.next.next
+
+        return dummy.next
