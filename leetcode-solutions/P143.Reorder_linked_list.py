f86f8ad5891b1560da6c083969cb4eb8c918a4ce
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-04-14 22:33:55 +0530

    P143

--- leetcode-solutions/P143.Reorder_linked_list.py
+++ leetcode-solutions/P143.Reorder_linked_list.py
@@ -43,6 +43,42 @@ class Solution(object):
             l1 = a
             cur = b
 
-            
-        
+# Method 2      
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        fast = head.next
+        slow = head
+
+        while fast and fast.next:
+            fast = fast.next.next            
+            slow = slow.next
+
+        middle = slow
+        middlenext = middle
+        prev = None
+
+        while middlenext:
+            nxt = middlenext.next
+            middlenext.next = prev
+            prev = middlenext
+            middlenext = nxt
+
+        a = head
+        p = prev
+
+        while a:
+            nxt = a.next
+            a.next = p
+            a = p
+            p = nxt
+
+        return head
         
