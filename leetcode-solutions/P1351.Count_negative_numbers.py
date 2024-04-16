0972b367a42b1050c0d509af09ff7ed524c8db5c
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-04-17 00:17:25 +0530

    P1351

--- leetcode-solutions/P1351.Count_negative_numbers.py
+++ leetcode-solutions/P1351.Count_negative_numbers.py
@@ -9,3 +9,27 @@ class Solution(object):
             less_than_zero += len(list(filter(lambda x: x < 0, grid[i])))
         
         return less_than_zero
+    
+# Method 2 
+class Solution:
+    def countNegatives(self, grid: List[List[int]]) -> int:
+        cnt =  0
+        def bs(n):
+            n.sort()
+            l = -1
+            r = len(n)-1
+
+            while l < r:
+                mid = l + (r-l+1)//2
+
+                if n[mid] >= 0:
+                    r = mid - 1
+                else:
+                    l = mid
+            
+            return l+1
+
+        for i in range(len(grid)):
+            cnt += bs(grid[i])
+
+        return cnt
