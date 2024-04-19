340f490685bf958c3b62fae19f11fc8ed04bd6a9
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-04-19 23:54:51 +0530

    P33

--- leetcode-solutions/P0033.Search_element_rotated_array.py
+++ leetcode-solutions/P0033.Search_element_rotated_array.py
@@ -105,3 +105,27 @@ class Solution:
                     h = mid - 1
 
         return -1
+    
+# almost same except '=' condition
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l = 0
+        r = len(nums) - 1
+
+        while l < r:
+            mid = l + (r-l)//2
+
+            if target == nums[mid]: return mid
+
+            if nums[l] <= nums[mid]:
+                if target >= nums[l] and target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            else:
+                if target <= nums[r] and target > nums[mid]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+
+        return -1 if nums[l] != target else l
