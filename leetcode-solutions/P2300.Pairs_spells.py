bf4b7dcc2f5b35be912d81ecac1d3a7155a31b72
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-04-21 19:56:52 +0530

    P2300

--- leetcode-solutions/P2300.Pairs_spells.py
+++ leetcode-solutions/P2300.Pairs_spells.py
@@ -10,3 +10,29 @@ class Solution:
             res[idx] = len(potions) - bisect_left(potions, m)
 
         return res
+
+# wrong submission
+class Solution:
+    def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
+        potions.sort()
+        
+        def bs(x):
+            l = -1
+            r = len(potions)
+
+            while l < r:
+                mid = l + (r-l)//2
+
+                if x * potions[mid] >= success:
+                    r = mid
+                else:
+                    l = mid + 1
+
+            return len(potions)-l if l >= 0 else 0
+
+        ar = []
+        for a in spells:
+            res = bs(a)
+            ar.append(res)
+
+        return ar
