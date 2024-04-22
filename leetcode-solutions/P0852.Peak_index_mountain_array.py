abe33c151794142d23c412903631ee416ea840e5
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-04-22 19:39:15 +0530

    P872

--- leetcode-solutions/P0852.Peak_index_mountain_array.py
+++ leetcode-solutions/P0852.Peak_index_mountain_array.py
@@ -14,3 +14,23 @@ class Solution(object):
                 r = mid
         
         return l
+
+# Method 2
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        l = 0
+        r = len(arr) - 1
+
+        while l < r:
+            mid = l + (r-l+1)//2
+
+            if 0 < mid < len(arr) - 1:
+                if arr[mid - 1] < arr[mid] > arr[mid+1]:
+                    return mid
+                else:
+                    if arr[mid] < arr[mid+1]:
+                        l += 1
+                    else:
+                        r -= 1
+            
+        return r
