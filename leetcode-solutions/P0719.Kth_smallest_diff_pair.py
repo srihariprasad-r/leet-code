0be427573c7a0f7276e92284b8315bc8f06957e4
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-04-23 22:42:39 +0530

    P719

---
+++ leetcode-solutions/P0719.Kth_smallest_diff_pair.py
@@ -0,0 +1,33 @@
+# wrong submission
+class Solution:
+    def smallestDistancePair(self, nums: List[int], k: int) -> int:
+        q = []
+        res = []
+        import copy
+
+        def combinations(idx, r, cnt):
+            if cnt == len(nums)-1: 
+                res.append(copy.deepcopy(r))
+                return
+                
+            for i in range(idx, len(nums)):
+                r.append(nums[i])
+                combinations(i+1, r, cnt + 1)
+                r.pop()
+
+            return res
+
+        c_arr = combinations(0, [], 0)
+        
+        heapq.heapify(q)
+        
+        for i in range(len(c_arr)):
+            diff = abs(c_arr[i][0]- c_arr[i][1])
+            heapq.heappush(q, diff)
+
+        c = 1
+        while c < k:
+            heapq.heappop(q)
+            c += 1
+
+        return heapq.heappop(q)
