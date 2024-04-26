3f88e4bfe076ef2d33f68ab7e56d59ab35e5de2d
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-04-27 01:13:30 +0530

    P1315

--- leetcode-solutions/P1315.Even_valued_grandparent.py
+++ leetcode-solutions/P1315.Even_valued_grandparent.py
@@ -24,3 +24,31 @@ class Solution:
                     stck.append((node.right, node, p))
 
         return self.s
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumEvenGrandparent(self, root: TreeNode) -> int:
+        def f(node):
+            if not node: return 0
+
+            c = 0
+            if not (node.val % 2):
+                if node.left:
+                    if node.left.left: c += node.left.left.val 
+                    if node.left.right: c += node.left.right.val         
+                if node.right:
+                    if node.right.left: c += node.right.left.val 
+                    if node.right.right: c += node.right.right.val    
+
+            lsum = f(node.left)
+            rsum = f(node.right)
+
+            return c + lsum + rsum
+
+        return f(root)
