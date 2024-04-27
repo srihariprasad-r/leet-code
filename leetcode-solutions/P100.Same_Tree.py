523dceee6aac3e1459cfacab2a11d26bdedc504c
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-04-27 23:07:52 +0530

    P100

--- leetcode-solutions/P100.Same_Tree.py
+++ leetcode-solutions/P100.Same_Tree.py
@@ -39,3 +39,22 @@ class Solution:
                 dfs(node1.left, node2.left) and dfs(node1.right, node2.right)
 
         return dfs(p, q)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        def f(node1, node2):
+            if not node1 and node2: return False
+            if node1 and not node2: return False
+
+            if not node1 and not node2: return True
+
+            return node1.val == node2.val and f(node1.left, node2.left) \
+                and f(node1.right, node2.right)
+
+        return f(p, q)
