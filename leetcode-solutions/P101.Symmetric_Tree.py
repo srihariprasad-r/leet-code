028c824411744c7c53aa061b7dd3147ca29e40d3
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-04-27 19:42:54 +0530

    P101

--- leetcode-solutions/P101.Symmetric_Tree.py
+++ leetcode-solutions/P101.Symmetric_Tree.py
@@ -52,3 +52,20 @@ class Solution:
 
         return dfs(root, root)
             
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        def f(node1, node2):
+            if not node1 and not node2: return True
+
+            if not node1 or not node2: 
+                return False
+         
+            return node1.val == node2.val and f(node1.left, node2.right) and f(node1.right, node2.left)
+
+        return f(root.left, root.right)
