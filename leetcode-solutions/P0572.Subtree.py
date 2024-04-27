249297f77e5d6a286884ad1934d21008da630454
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-04-27 23:44:56 +0530

    P572

--- leetcode-solutions/P0572.Subtree.py
+++ leetcode-solutions/P0572.Subtree.py
@@ -23,3 +23,35 @@ class Solution:
             return checkTree(node.left) or checkTree(node.right)
 
         return checkTree(root)
+    
+# wrong submission
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        def compare(node1, node2):
+            if not node1 and not node2: return True
+
+            if not node1 or not node2: return False
+
+            return node1.val == node2.val and compare(node1.left, node2.left) \
+                and compare(node1.right, node2.right)
+
+        def f(node1, node2):
+            if not node1 or not node2: return False
+
+            if node1.val == node2.val:
+                return compare(node1, node2)
+
+            left = False
+            right = False
+            left = f(node1.left, node2)
+            right = f(node1.right, node2)
+
+            return left or right
+
+        return f(root, subRoot)
