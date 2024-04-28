b85828676dff5e29dc01c9a0a2c911ba5cb778cd
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-04-28 17:30:49 +0530

    P2415

--- leetcode-solutions/P2415.Reverse_odd_levels_binary_tree.py
+++ leetcode-solutions/P2415.Reverse_odd_levels_binary_tree.py
@@ -19,3 +19,26 @@ class Solution:
         invert(root.left, root.right, 0)
 
         return root
+
+# wrong submission
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def reverseOddLevels(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        newRoot = None
+        def f(node, lvl):
+            if not node: return
+            newRoot = TreeNode(node.val)
+            if lvl % 2: 
+                newRoot.left = f(node.right, lvl + 1)
+                newRoot.right = f(node.left, lvl + 1)
+            else:
+                newRoot.left = f(node.left, lvl + 1)
+                newRoot.right = f(node.right, lvl + 1)  
+            return newRoot
+
+        return f(root, 1)
