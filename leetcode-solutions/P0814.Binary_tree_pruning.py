d65c98dd2eea71325177d52af405d7831f2a2fd9
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-04-29 19:33:36 +0530

    P814

--- leetcode-solutions/P0814.Binary_tree_pruning.py
+++ leetcode-solutions/P0814.Binary_tree_pruning.py
@@ -20,3 +20,33 @@ class Solution(object):
             return root
         else:
             return None
+
+# wrong submission
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pruneTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        rN = None
+        def prune(node):
+            if not node: return
+
+            rN = None
+            if node.val != 1 and not node.left and not node.right:
+                return rN
+
+            rN = TreeNode(node.val)
+            rN.left = prune(node.left)
+            if rN.left and rN.left.val != 1 and not node.left and not node.right:
+                return rN 
+            rN.right = prune(node.right)
+            if rN.right and rN.right.val != 1 and not node.left and not node.right:
+                return rN   
+
+            return rN
+
+        rN = prune(root)          
+        return prune(rN)    
