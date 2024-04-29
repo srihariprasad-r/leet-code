b0d98556a04917dcd118f99d3277bd9d7ea52395
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-04-29 23:00:55 +0530

    P1123

--- leetcode-solutions/P1123.Lowest_common_ancestor.py
+++ leetcode-solutions/P1123.Lowest_common_ancestor.py
@@ -25,3 +25,29 @@ class Solution(object):
                 return leftNode, leftDepth + 1
 
         return solution(root)[0]
+
+# wrong submission
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def lcaDeepestLeaves(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        def lca(node, parent, lvl):
+            if not node: return parent, lvl
+
+            if not node.left and not node.right:
+                return parent, lvl
+
+            pleft, leftlvl = lca(node.left, node, lvl+1)
+            pright, rightlvl = lca(node.right, node, lvl+1)
+
+            return pleft if leftlvl > rightlvl else pright , \
+                leftlvl if leftlvl > rightlvl else rightlvl
+
+        lparent, lvl1 =  lca(root.left, root, 1)
+        rparent, lvl2 =  lca(root.right, root, 1)
+
+        return lparent if lvl1 > lvl2 else rparent
