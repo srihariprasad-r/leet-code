591917bde97e3f8f0d09dc47c1d04ab32e973068
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-05-01 20:37:18 +0530

    P1110

---
+++ leetcode-solutions/P1110.Delete_nodes_forest.py
@@ -0,0 +1,28 @@
+# wrong submission
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def delNodes(self, root: Optional[TreeNode], to_delete: List[int]) -> List[TreeNode]:
+        res = []
+        def f(node):
+            if not node:
+                return None
+
+            # rN = TreeNode(node.val)
+            node.left = f(node.left)
+            node.right = f(node.right)    
+
+            if node.val in to_delete:
+                if node.left: res.append(node.left)
+                if node.right: res.append(node.right)
+                node = None     
+
+            return node
+
+        root = f(root)
+        res.append(root)
+        return res
