4d8951a0d7a1925c46ed19cd602cbe60fb6d884e
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-05-03 23:45:39 +0530

    P998

---
+++ leetcode-solutions/P0998.Maximum_binary_tree_II.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def insertIntoMaxTree(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if root and root.val > val:
+            root.right = self.insertIntoMaxTree(root.right, val)
+            return root
+
+        new = TreeNode(val)
+        new.left = root
+        return new
