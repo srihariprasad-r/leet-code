3caae8fab2aeb85eff24ee76e1a63c4934445c22
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-05-05 23:00:57 +0530

    P2104

--- leetcode-solutions/P2104.Sum_subarray_ranges.py
+++ leetcode-solutions/P2104.Sum_subarray_ranges.py
@@ -1,32 +1,13 @@
-# TLE
 class Solution:
     def subArrayRanges(self, nums: List[int]) -> int:
         s = 0
 
-        def getminvalue(arr):
-            stck = []
-            stck.append(arr[0])
-            for i in range(1, len(arr)):
-                while stck and stck[-1] > arr[i]: 
-                    stck.pop()
-                    stck.append(arr[i])
-
-            return stck[-1]
-
-        def getmaxvalue(arr):
-            stck = []
-            stck.append(arr[0])
-            for i in range(1, len(arr)):
-                while stck and stck[-1] < arr[i]:
-                    stck.pop()
-                    stck.append(arr[i])
-
-            return stck[-1]
-
         for i in range(len(nums)):
+            mn_val = float('inf')
+            mx_val = float('-inf')
             for j in range(i, len(nums)):
-                mn_val = getminvalue(nums[i:j+1])
-                mx_val = getmaxvalue(nums[i:j+1])
+                mn_val = min(mn_val, nums[j])
+                mx_val = max(mx_val, nums[j])
                 diff = mx_val - mn_val
                 s += diff
 
