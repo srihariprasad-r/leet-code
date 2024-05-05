c8971deb066faae9dbaf93dd51c5f7cb39e1debb
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-05-05 18:24:52 +0530

    P199

--- leetcode-solutions/P0199.Right_view_binary_tree.py
+++ leetcode-solutions/P0199.Right_view_binary_tree.py
@@ -28,3 +28,35 @@ class Solution(object):
         rightview(root, 0) 
         
         return [v for v in res.values()]
+
+# Method 2
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root: return []
+
+        q = deque()
+        res = []
+        st = set()
+
+        q.append((root,0))
+
+        while q:            
+            node, lvl = q.popleft()   
+
+            if node:  
+                if lvl not in st:                               
+                    res.append(node.val)                
+                    st.add(lvl)              
+    
+            if node.right: 
+                q.append((node.right, lvl+1))                
+            if node.left:
+                q.append((node.left, lvl+1))                              
+        
+        return res
