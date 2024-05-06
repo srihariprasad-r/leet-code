50ea6b4c1b22d6d9b41246fde49f9cb9461fe929
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-05-06 22:59:05 +0530

    P2104

--- leetcode-solutions/P2104.Sum_subarray_ranges.py
+++ leetcode-solutions/P2104.Sum_subarray_ranges.py
@@ -12,3 +12,46 @@ class Solution:
                 s += diff
 
         return s
+
+# wrong submission
+class Solution:
+    def subArrayRanges(self, nums: List[int]) -> int:
+        res = 0
+
+        ple = []
+        nle = []
+
+        leftMin = [0] * len(nums)
+        rightMin = [0] * len(nums)
+
+        for i in range(len(nums)):
+            while ple and nums[ple[-1]] > nums[i]:
+                ple.pop()
+            leftMin[i] = i - ple[-1] if ple else i + 1
+
+        for i in range(len(nums)-1, -1, -1):
+            while nle and nums[nle[-1]] > nums[i]:
+                nle.pop()
+            rightMin[i] = nle[-1] - i if nle else len(nums) - i
+
+        pge = []
+        nge = []
+
+        leftMax = [0] * len(nums)
+        rightMax = [0] * len(nums)
+
+        for i in range(len(nums)):
+            while pge and nums[pge[-1]] < nums[i]:
+                pge.pop()
+            leftMax[i] = i - pge[-1] if pge else i + 1
+
+        for i in range(len(nums)-1, -1, -1):
+            while nge and nums[nge[-1]] < nums[i]:
+                nge.pop()
+            rightMax[i] = nge[-1] - i if nge else len(nums) - i
+
+
+        for i in range(len(nums)):
+            res += nums[i]* (max(leftMax[i] , rightMax[i]) - min(rightMin[i] , leftMin[i]))
+
+        return res
