5c15e4c30b8badbd9fcfd03c6a563bb0137a588c
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-05-07 23:24:09 +0530

    P769

--- leetcode-solutions/P0769.Max_chunks_to_be_sorted.py
+++ leetcode-solutions/P0769.Max_chunks_to_be_sorted.py
@@ -11,3 +11,17 @@ class Solution:
                 cnt += 1
 
         return cnt
+
+# wromg submission
+class Solution:
+    def maxChunksToSorted(self, arr: List[int]) -> int:
+        stck = []
+        cnt = 0
+
+        for i in range(len(arr)):
+            while stck and stck[-1] > arr[i]:
+                cnt += 1
+                stck.pop()
+            stck.append(arr[i])
+        
+        return len(arr) - cnt
