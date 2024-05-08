6318df43b4392ca90077e31b1733a80ad8568df9
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-05-08 23:35:44 +0530

    P1673

--- leetcode-solutions/P1673.Competitive_subsequence.py
+++ leetcode-solutions/P1673.Competitive_subsequence.py
@@ -1,14 +1,14 @@
-# wrong submission
 class Solution:
     def mostCompetitive(self, nums: List[int], k: int) -> List[int]:
         stck = []
 
         for i in range(len(nums)):          
-            while stck and stck[-1] > nums[i]:
+            while stck and stck[-1] > nums[i] and len(stck) - 1  + len(nums) - i >=k:
                 stck.pop()
             stck.append(nums[i]) 
-                             
-        while len(stck) > k:
-            stck.pop()                            
+
+        if len(stck) > k:
+            while len(stck) > k:
+                stck.pop()                            
 
         return stck
