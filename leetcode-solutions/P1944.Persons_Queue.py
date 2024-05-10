6689f8a61461f6e664f5e8fd2adddc233a4c337f
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-05-10 20:41:54 +0530

    P1944

--- leetcode-solutions/P1944.Persons_Queue.py
+++ leetcode-solutions/P1944.Persons_Queue.py
@@ -13,3 +13,19 @@ class Solution:
             stck.append(i)
 
         return res
+
+# wrong submission
+class Solution:
+    def canSeePersonsCount(self, heights: List[int]) -> List[int]:
+        stck = []
+        nge = [0] * len(heights)
+
+        for i in range(len(heights)-1, -1, -1):
+            cnt = 1 if i < len(heights) - 1 else 0
+            while stck and stck[-1] < len(heights) - 1 and heights[stck[-1]] < heights[i]:              
+                stck.pop()
+                cnt += 1
+            stck.append(i)
+            nge[i] += cnt
+
+        return nge        
