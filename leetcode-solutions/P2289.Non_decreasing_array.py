2c229155f47e0d1dacdbe828f20766880ce80ef7
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-05-10 23:55:38 +0530

    P2289

--- leetcode-solutions/P2289.Non_decreasing_array.py
+++ leetcode-solutions/P2289.Non_decreasing_array.py
@@ -14,3 +14,16 @@ class Solution:
             stck.append(i)
         
         return res
+    
+# wrong submission
+class Solution:
+    def totalSteps(self, nums: List[int]) -> int:
+        stck = []
+        el = [0] * len(nums)
+
+        for i in range(len(nums)-1, -1, -1):
+            while stck and nums[stck[-1]] < nums[i]:
+                el[i] += el[stck.pop()] + 1                                
+            stck.append(i)
+
+        return max(el)
