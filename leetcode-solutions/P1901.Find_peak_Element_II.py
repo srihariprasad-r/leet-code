aa5e4c0ebaaa933facc2d220c778732705cc6285
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-05-14 19:51:04 +0530

    P1901

--- leetcode-solutions/P1901.Find_peak_Element_II.py
+++ leetcode-solutions/P1901.Find_peak_Element_II.py
@@ -30,3 +30,38 @@ class Solution:
                 l = mid + 1
             else:
                 h = mid - 1
+
+# wrong submission
+class Solution:
+    def findPeakGrid(self, mat: List[List[int]]) -> List[int]:
+        r = len(mat)
+        c = len(mat[0])
+
+        def bs(arr):
+            if len(arr) == 2: return 0 if arr[0] > arr[1] else 1
+            l = 0
+            r = len(arr)-1
+
+            while l < r:
+                mid = l + (r-l) // 2
+                
+                if arr[mid] > arr[mid-1] and arr[mid] > arr[mid+1]:
+                    return mid
+
+                if arr[mid-1] < arr[mid] and arr[mid] < arr[mid+1]:
+                    l = mid + 1
+                else:
+                    r = mid 
+
+            return l if arr[l] > arr[r] else r
+
+        mxval = float('-inf')
+        o_idx = []
+        for r in range(len(mat)):
+            midx = bs(mat[r])
+            v = mat[r][midx]
+            if v > mxval:
+                o_idx = [r, midx]
+                mxval = v
+
+        return o_idx
