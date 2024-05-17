f63fac71e7b4ea79b424408f7de978ce3291929f
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-05-18 01:02:19 +0530

    P113

--- leetcode-solutions/P0113.Path_Sum_II.py
+++ leetcode-solutions/P0113.Path_Sum_II.py
@@ -38,3 +38,38 @@ class Solution:
         dfs(root, targetSum, [])
 
         return res
+    
+# wrong submission
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        if not root: return []
+        
+        if root and not root.left and not root.right:
+            if root.val == targetSum: return [[root.val]]
+            
+        res = []
+        def f(node, l):
+            if not node: return
+            if node and not node.left and not node.right:
+                if l and sum(l) + node.val == targetSum: 
+                    l.append(node.val)
+                    res.append(copy.deepcopy(l))
+                return
+            
+            l.append(node.val)
+            f(node.left, l)
+            f(node.right, l)
+            l.pop()
+
+            return res
+
+        f(root.left, [root.val])
+        f(root.right, [root.val])
+
+        return res
