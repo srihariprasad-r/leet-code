8734c468d2f0e7367aa91ae1674e2f328f31469b
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-05-18 22:57:41 +0530

    P2856

--- leetcode-solutions/P2856.Mimimum_array_lengths_after_pair_removal.py
+++ leetcode-solutions/P2856.Mimimum_array_lengths_after_pair_removal.py
@@ -16,3 +16,21 @@ class Solution:
                 i -= 1
         
         return len(nums) - ans
+    
+# wrong submission
+class Solution:
+    def minLengthAfterRemovals(self, nums: List[int]) -> int:
+        stck = []
+        de = False
+        
+        stck.append(nums[0])
+        for i in range(1,len(nums)-1):
+            while stck and stck[-1] < nums[i]:
+                stck.pop()
+                de = True
+            if de and stck: stck.pop()
+            stck.append(nums[i])
+        
+        if stck and stck[-1] < nums[-1]: stck.pop()
+        
+        return len(stck)
