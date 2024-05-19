1859f4c9c555a283aff3604a8848a56412b2529d
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-05-19 20:32:32 +0530

    P312

--- leetcode-solutions/P0312.Burst_ballons.py
+++ leetcode-solutions/P0312.Burst_ballons.py
@@ -30,16 +30,17 @@ class Solution:
         mx = float('-inf')
 
         for g in range(len(dp)):
-            i = 0
-            for j in range(g,len(dp)):                
+            i = 0            
+            for j in range(g,len(dp)):              
                 for k in range(i, j+1):
                     left = dp[i][k-1] if k != i else 0
                     right = dp[k+1][j] if k != j else 0
-                    val = left + right + (arr[i-1] if i > 0 else 1* arr[k]*arr[j+1] if j < len(arr)-1 else 1)
+                    val = left + right + ((arr[i-1] if i > 0 else 1)* (arr[k])*(arr[j+1] if j < len(arr)-1 else 1))
 
                     if val > mx: mx = val
 
                 dp[i][j] = mx
                 i += 1
+
 
         return dp[0][len(arr)-1]
