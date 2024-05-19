6a1e7d0135e6cb0ce771f7319dcf681a362c5efa
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-05-19 21:59:51 +0530

    P312

--- leetcode-solutions/P0312.Burst_ballons.py
+++ leetcode-solutions/P0312.Burst_ballons.py
@@ -23,24 +23,26 @@ class Solution:
             
         return dp[0][len(arr)-1]
 
-# wrong submission
 class Solution:
     def maxCoins(self, arr: List[int]) -> int:
         dp = [[0 for _ in range(len(arr))] for _ in range(len(arr))]
-        mx = float('-inf')
 
         for g in range(len(dp)):
-            i = 0            
-            for j in range(g,len(dp)):              
+            i = 0
+            j = g    
+            while j < len(dp[0]):       
+                mx = float('-inf')                
                 for k in range(i, j+1):
-                    left = dp[i][k-1] if k != i else 0
+                    left = dp[i][k-1] if k != i  else 0
                     right = dp[k+1][j] if k != j else 0
-                    val = left + right + ((arr[i-1] if i > 0 else 1)* (arr[k])*(arr[j+1] if j < len(arr)-1 else 1))
+                    val = (arr[i-1] if i > 0 else 1)* (arr[k])*(arr[j+1] if j < len(arr)-1 else 1)
 
-                    if val > mx: mx = val
+                    total = left + right + val
+
+                    if total > mx: mx = total
 
                 dp[i][j] = mx
+                j += 1
                 i += 1
 
-
-        return dp[0][len(arr)-1]
+        return dp[0][-1]
