8bfde876c456d59bf6949e060198eaab7fc9e871
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-06-05 22:59:46 +0530

    P689

--- leetcode-solutions/P0689.Max_sum_3_non_overlapping_subarray.py
+++ leetcode-solutions/P0689.Max_sum_3_non_overlapping_subarray.py
@@ -58,34 +58,33 @@ class Solution:
         pleft = [0] * len(nums)
         pright = [len(nums)-k] * len(nums)
 
-        p = [0] * len(nums)
-        p[0] = nums[0]
+        p = [0] * (len(nums)+1)
 
         for i in range(1, len(nums)):
-            p[i] = p[i-1] + nums[i]
+            p[i+1] = p[i] + nums[i-1]
 
-        total = nums[k] - nums[0]
+        total = p[k] - p[0]
         for i in range(k, len(nums)):
-            s = nums[i-k] - nums[i]
+            s = p[i+1] - p[i+1-k]
             if s > total:
                 total = s
-                pleft[i] = i - k
+                pleft[i] = i - k+1
             else:
                 pleft[i] = pleft[i-1]
 
-        total = nums[len(nums)-1] - nums[len(nums)-1-k]
+        total = p[len(nums)] - p[len(nums)-k]
         for i in range(len(nums)-k-1, -1, -1):
-            s = nums[i+k] - nums[i]
+            s = p[i+k] - p[i]
             if s >= total:
                 total = s
-                pright[i] = i + k
+                pright[i] = i
             else:
                 pright[i] = pright[i+1]
 
-        for i in range(k, len(nums)-(2*k)):
+        for i in range(k, (len(nums)-2*k+1)):
             l = pleft[i-1]
             r = pright[i+k]
-            total = nums[i+k] - nums[i] + nums[l+k] - nums[l] + nums[r+k] - nums[r]
+            total = p[i+k] - p[i] + p[l+k] - p[l] + p[r+k] - p[r]
             if total >= ms:
                 ans = [l, i, r]
                 ms = total
