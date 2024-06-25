98f92a72c96693d2857f3947b9ab705b67a34785
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-06-25 18:09:47 +0530

    P552

--- leetcode-solutions/P0552.Attendance_record_II.py
+++ leetcode-solutions/P0552.Attendance_record_II.py
@@ -12,3 +12,24 @@ class Solution:
             return a + l + p
 
         return solve(n, 0, 0)
+
+# wrong submission
+class Solution:
+    def checkRecord(self, n: int) -> int:
+        dp = [[[-1 for _ in range(3)] for _ in range(2)] for _ in range(n+1)]
+
+        def solve(n, absent, consecutivelate):
+            if ((absent >= 2) or (consecutivelate >= 3)): return 0
+            if n == 0: return 1            
+            
+            if dp[n][absent][consecutivelate] != -1: return dp[n][absent][consecutivelate]
+
+            a = solve(n-1, absent + 1, 0)  % 100_000_007   
+            l = solve(n-1, absent, consecutivelate + 1)  % 100_000_007   
+            p = solve(n-1, absent, 0)  % 100_000_007                
+
+            dp[n][absent][consecutivelate] = (a + l + p) % 100_000_007            
+            
+            return dp[n][absent][consecutivelate]
+
+        return solve(n, 0, 0)
