9f60cf54fa8fa33c1cc37eac6ea2cb75b76230ce
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-07-29 17:11:50 +0530

    P518

--- leetcode-solutions/P0518.Coin_change_II.py
+++ leetcode-solutions/P0518.Coin_change_II.py
@@ -19,3 +19,19 @@ class Solution:
                 dp[i][j] = include + exclude
 
         return dp[-1][-1] if dp[-1][-1] > -1 else 0
+
+
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        def recursion(idx, tot):
+            if tot < 0 or idx < 0: return 0
+            if tot == 0: return 1
+
+            pick = 0
+            if coins[idx] <= tot:
+                pick = recursion(idx,tot-coins[idx])
+            no_pick = recursion(idx-1, tot)
+
+            return pick + no_pick
+
+        return recursion(len(coins)-1, amount)            
