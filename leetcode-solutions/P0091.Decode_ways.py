70fc89e79bcb55622c52bc60cf96a3d4e695e11a
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-07-31 09:24:22 +0530

    P91

--- leetcode-solutions/P0091.Decode_ways.py
+++ leetcode-solutions/P0091.Decode_ways.py
@@ -28,3 +28,29 @@ class Solution(object):
 
         dp = [-1] * len(s)
         return dfs(0, dp)
+    
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        if s[0] == '0': return 0
+        dp = [0 for _ in range(len(s))]
+
+        def recursion(idx):
+            if idx == len(s): return 1
+
+            one_digit = 0
+            two_digit = 0
+
+            if dp[idx] != 0: return dp[idx]
+
+            if '1' <= s[idx] <= '9':
+                one_digit += recursion(idx+1)
+
+            if idx < len(s) - 1:
+                if ((s[idx] == '1' and ('0' <= s[idx+1] <= '9')) or (s[idx] == '2' and ('0' <= s[idx+1] <= '6'))):
+                    two_digit += recursion(idx+2)
+
+            dp[idx] = one_digit + two_digit
+
+            return one_digit + two_digit
+
+        return recursion(0)            
