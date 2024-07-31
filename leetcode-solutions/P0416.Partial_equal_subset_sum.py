c71890ac70669445791a3a250d7374c5399cd1a7
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-07-31 19:44:21 +0530

    P416

--- leetcode-solutions/P0416.Partial_equal_subset_sum.py
+++ leetcode-solutions/P0416.Partial_equal_subset_sum.py
@@ -76,3 +76,31 @@ class Solution:
                     dp[i][j] = dp[i-1][j] or dp[i-1][j-nums[i-1]]
 
         return dp[-1][-1]
+    
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        s = sum(nums)
+        partial = s // 2
+
+        def recursion(idx, st, partial, k):
+            if idx > len(nums) -1:
+                if k == 2: return True
+                return False
+
+            if k == 2: return True
+
+            if st == partial or nums[idx] == partial: 
+                k = k + 1
+                st = 0
+
+            for i in range(idx, len(nums)):
+                st += nums[i]                
+                if st > partial:
+                    return recursion(i+1, st - nums[i], partial, k)
+                else:
+                    return recursion(i+1, st, partial, k)
+                st -= nums[i]
+
+            return False
+
+        return recursion(0, 0, partial, 0)            
