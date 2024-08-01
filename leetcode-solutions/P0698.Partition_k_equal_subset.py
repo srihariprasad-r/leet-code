57bfdb909a3d40894439d8d41bb0ac05cd61cdf4
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-08-01 18:48:24 +0530

    P698

--- leetcode-solutions/P0698.Partition_k_equal_subset.py
+++ leetcode-solutions/P0698.Partition_k_equal_subset.py
@@ -22,3 +22,34 @@ class Solution:
             return False
 
         return recursion(0, 0, k)
+
+class Solution:
+    def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
+        s = sum(nums)
+        if s % k: return False
+
+        need = s // k
+
+        def recursion(idx, st, s_k):
+            if idx >= len(nums): return False
+            
+            if idx == len(nums) - 1 and st + nums[idx] == need: 
+                return True
+
+            if st == need:
+                return recursion(idx+1, 0, s_k + 1)
+
+            if s_k == k:
+                return True
+
+            take = False
+            if st + nums[idx] <= need:
+                st += nums[idx]
+                take = recursion(idx+1, st, s_k+1)
+            else:
+                take = recursion(idx+1, 0, s_k)
+            #no_take = recursion(idx+1, st, s_k)
+
+            return take
+
+        return recursion(0, 0, 0)        
