a8a9d9d684448f3fc9c43640c1cde1f86f3bbaa5
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-08-02 19:46:14 +0530

    P698

--- leetcode-solutions/P0698.Partition_k_equal_subset.py
+++ leetcode-solutions/P0698.Partition_k_equal_subset.py
@@ -28,28 +28,34 @@ class Solution:
         s = sum(nums)
         if s % k: return False
 
-        need = s // k
+        need = int(s / k)
+        self.groups = 0
+
+        visited = [0] * len(nums)
 
         def recursion(idx, st, s_k):
-            if idx >= len(nums): return False
-            
-            if idx == len(nums) - 1 and st + nums[idx] == need: 
-                return True
+            if idx == len(nums) - 1:
+                if st + nums[idx] == need: 
+                    s_k += 1
+                    self.groups += 1
+                # if s_k == k: return True
+                return False
+ 
+            if st == need: return True
 
-            if st == need:
-                return recursion(idx+1, 0, s_k + 1)
+            for i in range(len(nums)):
+                if not visited[i] and st + nums[i] <= need:
+                    visited[i] = 1
+                    st += nums[i]
+                    if st == need: 
+                        s_k += 1
+                        st = 0
+                    if recursion(i+1, st, s_k): 
+                        self.groups += 1
+                        return True
+                    st -= nums[i]
+                    visited[i] = 0
 
-            if s_k == k:
-                return True
+            return self.groups == k
 
-            take = False
-            if st + nums[idx] <= need:
-                st += nums[idx]
-                take = recursion(idx+1, st, s_k+1)
-            else:
-                take = recursion(idx+1, 0, s_k)
-            #no_take = recursion(idx+1, st, s_k)
-
-            return take
-
-        return recursion(0, 0, 0)        
+        return recursion(0, 0, 0)                  
