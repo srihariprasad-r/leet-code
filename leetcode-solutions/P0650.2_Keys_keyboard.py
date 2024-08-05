ca2f8078c03046830a0d8b61184b1ddae124637b
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-08-05 17:55:47 +0530

    P650

--- leetcode-solutions/P0650.2_Keys_keyboard.py
+++ leetcode-solutions/P0650.2_Keys_keyboard.py
@@ -12,3 +12,22 @@ class Solution:
             return min(take, no_take)
         
         return dfs(1, 0)
+
+class Solution:
+    def minSteps(self, n: int) -> int:
+        def recursion(idx, c):
+            if idx > n or c < 0: return 0
+            
+            take = float('inf')
+            no_take = float('inf')
+
+            if idx < c:
+                take = 1 + recursion(idx+1, c//idx)
+            if idx > c:
+                no_take = 1 + recursion(idx+1, c)
+
+            return min(take, no_take) 
+
+        res = recursion(1, n)
+
+        return 0 if res == float('inf') else res        
