25058e1113c0204c0e523276b0ec3afe56ed9267
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-08-09 17:09:35 +0530

    P279

--- leetcode-solutions/P0279.Perfect_Squares.py
+++ leetcode-solutions/P0279.Perfect_Squares.py
@@ -62,3 +62,21 @@ class Solution:
                         visited.add(s+squares[i])
 
         return d 
+
+
+class Solution:
+    def numSquares(self, n: int) -> int:
+        dp = [float('inf')] * (n+1)
+
+        dp[1] = 1
+
+        for i in range(2, n+1):
+            for j in range(i):
+                if j * j == i: 
+                    dp[i] = 1
+
+        for i in range(1, n+1):
+            for j in range(1, int(i**0.5)+1):
+                dp[i] = min(dp[i], dp[i-j*j] + 1)
+
+        return dp[-1]
