890df0674bc0df4a38980a273f82c5a709677c19
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-08-15 00:36:02 +0530

    P97

--- leetcode-solutions/P0097.Interleaving_string.py
+++ leetcode-solutions/P0097.Interleaving_string.py
@@ -1,22 +1,20 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
         if len(s1) + len(s2) > len(s3): return False
-        
+
         def recursion(i, j, k, st):
             if  k >= len(s3): 
                 if st == s3: return True
                 return False
 
-            if i >= len(s1) and j < len(s2): return False
-            if i < len(s1) and j >= len(s2): return False
+            a = False
+            b = False
 
-            take = False
+            if i < len(s1):
+                a = recursion(i+1, j, k+1, st + s1[i] if s1[i] == s3[k] else st)            
+            if j < len(s2):
+                 b = recursion(i, j+1, k+1, st + s2[j] if s2[j] == s3[k] else st)
+            
+            return False if not a and not b else True
 
-            if s1[i] == s3[k]:
-                return recursion(i+1, j, k+1, st + s1[i])
-            else:
-                return recursion(i, j+1, k+1, st + s2[j])
-
-            return False
-
-        return recursion(0,0,0,'')            
+        return recursion(0,0,0,'')          
