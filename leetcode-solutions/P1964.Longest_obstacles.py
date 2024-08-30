dad58e8e37f18d1c91a1a4e4a7e98dbdb7a86d25
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-08-30 20:12:10 +0530

    P1964

--- leetcode-solutions/P1964.Longest_obstacles.py
+++ leetcode-solutions/P1964.Longest_obstacles.py
@@ -1,11 +1,30 @@
 class Solution:
     def longestObstacleCourseAtEachPosition(self, obstacles: List[int]) -> List[int]:
-        dp = [1] * len(obstacles)
+        dp = [float('inf')] * len(obstacles)
+        res = [1] * len(obstacles)
+
+        dp[0] = obstacles[0]
+
+        def bs(arr, tgt):
+            l = 0
+            r = len(arr)-1
+
+            while l < r:
+                mid = l + (r-l) // 2
+                if arr[mid] <= tgt:
+                    l = mid + 1
+                else:
+                    r = mid-1
+
+            return l
 
         for i in range(1, len(obstacles)):
-            cnt = 0
-            for j in range(0,i):
-                if obstacles[i] >= obstacles[j]:
-                    dp[i] = max(dp[i], dp[j]+1) 
+            if obstacles[i] >= dp[i-1]:
+                dp[i] = obstacles[i]
+                res[i] = i+1
+            else:
+                idx = bs(dp, obstacles[i])
+                dp[idx] = obstacles[i]
+                res[i] = max(res[i], idx+1)
 
-        return dp
+        return res
