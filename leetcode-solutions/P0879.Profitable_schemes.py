8766f7937429f9990eb91c8d35f295a7ee7bacef
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-09-29 19:07:01 +0530

    P879

--- leetcode-solutions/P0879.Profitable_schemes.py
+++ leetcode-solutions/P0879.Profitable_schemes.py
@@ -1,15 +1,17 @@
 class Solution:
     def profitableSchemes(self, n: int, minProfit: int, group: List[int], profit: List[int]) -> int:
-        def recursion(idx, ct):
-            if idx >= len(profit): return 0
+        def recursion(idx, ct, pt):
+            if ct > n: return 0
+
+            if idx == len(profit): 
+                if pt >= minProfit: return 1
+                return 0
 
             take = 0
             no_take = 0
-            if ((ct + group[idx] <= n) and (profit[idx] >= minProfit)):
-                take = 1 + recursion(idx, ct + group[idx])
-            else:
-                no_take = recursion(idx+1, 0)
+            take = recursion(idx+1, ct + group[idx], min(pt + profit[idx], minProfit))
+            no_take = recursion(idx+1, ct, pt)
 
             return take + no_take
 
-        return recursion(0, 0)        
+        return recursion(0, 0, 0)        
