9697f7349f350d83678ad9678350028500501c28
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2024-10-19 20:24:22 +0530

    P1911

--- leetcode-solutions/P1911.Maximum_alternating_sum.py
+++ leetcode-solutions/P1911.Maximum_alternating_sum.py
@@ -1,22 +1,17 @@
 class Solution:
     def maxAlternatingSum(self, nums: List[int]) -> int:
         msum = float('-inf')
-        def recurse(idx, arr, flag, o_sum, e_sum, msum):    
-            if idx >= len(arr):
-                return msum
+        def recurse(idx, flag):
+            if idx >= len(nums):
+                return 0
 
-            take = 0
-            no_take = 0
-            if not flag:
-                if idx % 2: o_sum += nums[idx]
-                else: e_sum += nums[idx]
-                msum = max(msum, abs(o_sum - e_sum))
-                take = recurse(idx+1, arr, flag, o_sum, e_sum, msum)
-            else:
-                arr = arr[idx:]
-                msum = max(msum, abs(o_sum - e_sum))                
-                no_take = recurse(idx,arr, not flag, o_sum, e_sum, msum)
+            c = nums[idx]
+            if flag : c *= -1
+
+            take = recurse(idx+1, not flag) + c
+
+            no_take = recurse(idx+1, flag)
 
             return max(take, no_take)
 
-        return recurse(0, nums, 0, 0, 0, msum)            
+        return recurse(0, False)            
